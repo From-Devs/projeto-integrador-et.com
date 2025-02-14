0cb23566a9ae7603c69d34411410bd723e47324c
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-02-14 17:08:36 -0400

    push basico

---
+++ et_pontocom/app/usuario - MVC/view/teste.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ . "/../../../public/componentes/header.php"
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Et.com</title>
+    <link rel="stylesheet" href="../../../public/css/header.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div id="header"></div>
+    <h1>Bungas</h1>
+
+    <!-- Scripts -->
+    <script src="../../../public/javascript/header.js"></script>
+</body>
+</html>
