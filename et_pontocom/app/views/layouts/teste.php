52f553a274b634bbfa1be6166f981a1899e7fe97
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-02-17 14:17:51 -0400

    juntando 1

---
+++ et_pontocom/app/views/layouts/teste.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . "/../../../public/componentes/header.php"
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Et.com</title>
+    <link rel="stylesheet" href="../../../public/css/header.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <!-- Scripts -->
+    <script src="../../../public/javascript/header.js"></script>
+</body>
+</html>
