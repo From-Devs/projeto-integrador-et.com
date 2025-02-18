1c9811698bec0db442a3cc15430e21bd83a4f26d
Ryan Gosling <elielmurbach01@gmail.com>
2025-02-18 17:16:29 -0400

    Rodapé quase prontosksksksk
    seila

---
+++ et_pontocom/app/views/layouts/teste.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . "/../../../public/componentes/Rodapé.php"
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>et_pontocom</title>
+    <link rel="stylesheet" href=" ../../../public/css/Rodape.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <?php echo Rodape()?>
+
+</body>
+</html>
