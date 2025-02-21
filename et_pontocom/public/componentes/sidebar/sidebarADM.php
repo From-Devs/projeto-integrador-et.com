e7b07ab44a6165afd81aac2fc22cacae8b8dc347
Nicolle Zaleski <nicolle.m.zaleski@gmail.com>
2025-02-21 15:35:41 -0400

    Estilização breve da sidebar

--- et_pontocom/public/componentes/sidebar/sidebarADM.php
+++ et_pontocom/public/componentes/sidebar/sidebarADM.php
@@ -4,50 +4,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../css/sidebar/sidebar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.4/css/boxicons.min.css">
-    <link rel="stylesheet" href="../../css/sidebarAdmCSS/sidebar.css">
+    
 </head>
 <body>
     <div class="sidebar_adm">
         <nav class="nav_adm">
             <div class="logo">
-                <img src="../../imagens/LogoPreta1.png" alt="">
+                <img src="../../imagens/LogoBranca1.png" alt="">
             </div>
             <div class="linhaGradiente"></div>
             <div class="botoesAdm">
                 <ul>
-                    <li class="button_dashboard"><a href="">
+                    <li><a href="#" class="button_dashboard">
                         <span class='bx bxs-home' ></span>
                         <span>Dashboard</span>
                     </a></li>
-                    <li class="button_produtos"><a href="">
+                    <li><a href="#" class="button_produtos">
                         <span class='bx bxs-package'></span>
                         <span>Produtos</span>
                     </a></li>
-                    <li class="button_pedidos"><a href="">
+                    <li><a href="#" class="button_pedidos">
                         <span class='bx bx-money-withdraw' ></span>
                         <span>Pedidos</span>
                     </a></li>
-                    <li class="button_associados"><a href="">
+                    <li><a href="#" class="button_associados">
                         <span class="fa-solid fa-users" style="font-size: 13px;"></span>
                         <span>Associados</span>
                     </a></li>
-                    <li class="button_relatorios"><a href="">
+                    <li><a href="#" class="button_relatorios">
                         <span class='bx bxs-receipt'></span>
                         <span>Relatórios</span>
                     </a></li>
-                    <li class="button_customizacao"><a href="">
+                    <li><a href="#" class="button_customizacao">
                         <span class='bx bxs-palette'></span>
                         <span>Customização</span>
                         
                     </a></li>
                 </ul>
             </div>
-            <div class="button-sair">
+            <div><a href="#" class="button-sair">
                 <span class='bx bx-log-out'></span>
                 <span>Sair</span>
-            </div>
+            </a></div>
 
         </nav>
     </div>
