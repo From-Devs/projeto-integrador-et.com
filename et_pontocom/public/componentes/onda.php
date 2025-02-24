904df0c076b5914b0f2d312341399e0217e68d89
GuilhermeNantes <guinantes27@gmail.com>
2025-02-24 17:15:31 -0400

    sdfsdfsfdf
    dsfsdfsfsdf

---
+++ et_pontocom/public/componentes/onda.php
@@ -0,0 +1,21 @@
+<style>
+    .wave-container {
+        position: relative;
+        width: 100%;
+        height: 120px;
+        background-color: #000; /* Cor de fundo */
+        overflow: hidden;
+    }
+    
+
+
+</style>
+
+<div class="wave-container">
+    <div class="wave1">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ededed" fill-opacity="1" d="M0,96L80,122.7C160,149,320,203,480,192C640,181,800,107,960,80C1120,53,1280,75,1360,85.3L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+    </div>
+    <div class="wave2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#dcdcdc" fill-opacity="1" d="M0,96L80,122.7C160,149,320,203,480,192C640,181,800,107,960,80C1120,53,1280,75,1360,85.3L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+    </div>
+</div>
