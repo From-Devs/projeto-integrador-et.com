51b0e8af575274c6758facaf7b3caebe642bd1bb
GuilhermeNantes <guinantes27@gmail.com>
2025-02-25 17:18:15 -0400

    ondas mais tirar
    eu adicioanei  a duas ondas e quando for eu vou ter que mudar as pasta

--- et_pontocom/public/componentes/onda.php
+++ et_pontocom/public/componentes/onda.php
@@ -1,21 +1,68 @@
 <style>
-    .wave-container {
+
+    body {
+        /* ondas */
+        background-image: linear-gradient(to top, #EBC1B6, #C69065);
+    }
+    section
+    {   
         position: relative;
         width: 100%;
-        height: 120px;
-        background-color: #000; /* Cor de fundo */
+        height: 100vh;
         overflow: hidden;
+        
+    }
+    /* aqui é onde criar a onda do header */
+    section .wave
+    {
+        position: absolute;
+        /* top: 45%;
+        left: 2%; */
+        width: 100%;
+        height: 100px;
+        background: url(../imagens/wave.png);
+        background-size: 1000px 100px;
+    }
+    /* onde inicia a animaçao pls nao mexer */
+    section .wave.wave1 
+    {
+        top: 10;
+        animation: animacao 10s linear infinite;
+        z-index: 1000;
+        opacity: 1;
+        animation-delay: 0s;
+
+
     }
     
+    section .wave.wave2
+    {
+
+        top: 0;
+        animation: animacao 8s linear infinite;
+        z-index: 999;
+        opacity: 0.2;
+        animation-delay: 0s;
 
 
+    }
+    /* fim da animação */
+    @keyframes animacao
+    {
+        0%
+        {
+            background-position-x: 0;
+        }
+        100%
+        {
+            background-position-x: -1000px;
+        }
+    }
+        
 </style>
-
 <div class="wave-container">
-    <div class="wave1">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ededed" fill-opacity="1" d="M0,96L80,122.7C160,149,320,203,480,192C640,181,800,107,960,80C1120,53,1280,75,1360,85.3L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-    </div>
-    <div class="wave2">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#dcdcdc" fill-opacity="1" d="M0,96L80,122.7C160,149,320,203,480,192C640,181,800,107,960,80C1120,53,1280,75,1360,85.3L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-    </div>
+    <section>
+        <div class="wave wave1"></div>
+        <div class="wave wave2"></div>
+    </section>
 </div>
