ef70db22521c422ec8c2e5651aea46356d25b53d
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-02-26 13:16:39 -0400

    Update teste.php

--- et_pontocom/app/views/usuario/teste.php
+++ et_pontocom/app/views/usuario/teste.php
@@ -12,10 +12,9 @@
     <title>Et.com</title>
     <link rel="stylesheet" href="../../../public/css/header.css">
     <link rel="stylesheet" href="../../../public/css/testeSidebar.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../../../public/css/teste.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Pixelify+Sans:wght@400..700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
 </head>
 <body>
