aa000d92de0b13ba7d111041f05dcd83d70ab358
Marques Evandro <evandrocastromarques@gmail.com>
2025-02-27 16:45:26 -0400

    commit

---
+++ et_pontocom/app/views/layouts/adm/dashboard-associado.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>    
+    <link rel="stylesheet" href="<?php $_SERVER["DOCUMENT_ROOT"];?>et_pontocom/public/css/sidebar/sidebarA.css">
+
+</head>
+<body>
+    <?php
+        require "../../public/componentes/sidebar/sidebarAssoc.php"
+    ?>  
+    
+</body>
+</html>
