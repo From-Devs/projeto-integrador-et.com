eed8f84104bb4621bb5d836547d61968b641c04e
NicolleZaleski <nicolle.m.zaleski@gmail.com>
2025-03-06 20:46:18 -0300

    sidebar da header feito em casa

--- et_pontocom/public/componentes/sidebarHeader.php
+++ et_pontocom/public/componentes/sidebarHeader.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/sidebarHeader.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.4/css/boxicons.min.css">
+    <link rel="stylesheet" href="../css/SidebarHeader.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
 </head>
 <body>
@@ -21,103 +21,117 @@
 
 ?> -->
 
-<div class="sidebar_adm">
-    <div class="nav_adm">
-        <div class="logo">
-            <img src="../imagens/LogoBranca2.png" alt="">
+    <div class="sidebar_adm">
+        <div class="nav_adm">
+            <a href="#" class="logo">               <!-- Adicionar o atalho para a página principal -->
+                <img src="../imagens/LogoBranca2.png" alt="">
+            </a>
+            <div class="botoes_sidebar">
+                <div class="botoes_categoria">
+                    <ul>
+                        <p class="categoria_nome">Categorias</p>
+                        <li class="categoria">
+                            <div>
+                                <a href="#" class="categoria_link">Maquiagem</a>
+                                <div class="button_sub">
+                                    <i class='fa fa-chevron-down'></i>
+                                </div>
+                            </div>
+                            <ul class="submenu">
+                                <li><a href="#" class="submenu_link">Pele</a></li>
+                                <li><a href="#" class="submenu_link">Olhos</a></li>
+                                <li><a href="#" class="submenu_link">Boca</a></li>
+                                <li><a href="#" class="submenu_link">Sobrancelhas</a></li>
+                            </ul>
+                        </li>
+                        <li class="categoria">
+                            <div>
+                                <a href="#" class="categoria_link">Perfume</a>
+                                <div class="button_sub">
+                                    <i class='fa fa-chevron-down'></i>
+                                </div>
+                            </div>
+                            <ul class="submenu">
+                                <li><a href="#" class="submenu_link">Feminino</a></li>
+                                <li><a href="#" class="submenu_link">Masculino</a></li>
+                                <li><a href="#" class="submenu_link">Unissex</a></li>
+                            </ul>
+                        </li>
+                        <li class="categoria">
+                            <div>
+                                <a href="#" class="categoria_link">SkinCare</a>
+                                <div class="button_sub">
+                                    <i class='fa fa-chevron-down'></i>
+                                </div>
+                            </div>
+                            <ul class="submenu">
+                                <li><a href="#" class="submenu_link">Limpeza</a></li>
+                                <li><a href="#" class="submenu_link">Esfoliação</a></li>
+                                <li><a href="#" class="submenu_link">Hidratação</a></li>
+                                <li><a href="#" class="submenu_link">Máscara</a></li>
+                                <li><a href="#" class="submenu_link">Protetor Solar</a></li>
+                                <li><a href="#" class="submenu_link">Especiais</a></li>
+                            </ul>
+                        </li>
+                        <li class="categoria">
+                            <div>
+                                <a href="#" class="categoria_link">Cabelo</a>
+                                <div class="button_sub">
+                                    <i class='fa fa-chevron-down'></i>
+                                </div>
+                            </div>
+                            <ul class="submenu">
+                                <li><a href="#" class="submenu_link">Dia-a-dia</a></li>
+                                <li><a href="#" class="submenu_link">Tratamentos</a></li>
+                                <li><a href="#" class="submenu_link">Estilização</a></li>
+                                <li><a href="#" class="submenu_link">Especiais</a></li>
+                                <li><a href="#" class="submenu_link">Acessórios</a></li>
+                            </ul>
+                        </li>
+                        <li class="categoria">
+                            <div>
+                                <a href="#" class="categoria_link">Eletrônicos</a>
+                                <div class="button_sub">
+                                    <i class='fa fa-chevron-down'></i>
+                                </div>
+                            </div>
+                            <ul class="submenu">
+                                <li><a href="#" class="submenu_link">Cabelo</a></li>
+                                <li><a href="#" class="submenu_link">Pincel</a></li>
+                                <li><a href="#" class="submenu_link">Esponja</a></li>
+                            </ul>
+                        </li>
+                        <li class="categoria">
+                            <div>
+                                <a href="#" class="categoria_link">Corporal</a>
+                                <div class="button_sub">
+                                    <i class='fa fa-chevron-down'></i>
+                                </div>
+                            </div>
+                            <ul class="submenu">
+                                <li><a href="#" class="submenu_link">Body Splash</a></li>
+                                <li><a href="#" class="submenu_link">Óleos</a></li>
+                                <li><a href="#" class="submenu_link">Cremes</a></li>
+                                <li><a href="#" class="submenu_link">Protetores</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="botoes_config">
+                    <p class="categoria_nome">Ajuda e Configurações</p>
+                    <ul>
+                        <li class="config"><a href="#" class="config_link">Minha Conta</a></li>
+                        <li class="config"><a href="#" class="config_link">Área de Administração</a></li>
+                        <li class="config"><a href="" class="config_link">Meus Pedidos</a></li>
+                        <li class="config"><a href="" class="config_link">Associados</a></li>
+                        <li class="config"><a href="" class="config_link">Termos de Uso e Privacidade</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
+        
     </div>
-    <div class="botoes_sidebar">
-        <div class="botoes_categoria">
-            <ul>
-                <p class="categoria_nome">Categorias</p>
-                <li class="categoria"><a href="" class="categoria_link">
-                    <div class="categoria_titulo">
-                        <span class="nome_titulo">Maquiagem</span>
-                        <span class='bx bx-chevron-down'></span>
-                    </div>
-                    <ul class="submenu">
-                        <li><a href="#" class="submenu_link">Pele</a></li>
-                        <li><a href="#" class="submenu_link">Olhos</a></li>
-                        <li><a href="#" class="submenu_link">Boca</a></li>
-                        <li><a href="#" class="submenu_link">Sobrancelhas</a></li>
-                    </ul>
-                </a></li>
-                <li class="categoria"><a href="" class="categoria_link">
-                <div class="categoria_titulo">
-                        <span class="nome_titulo">Perfume</span>
-                        <span class='bx bx-chevron-down'></span>
-                    </div>
-                    <ul class="submenu">
-                        <li><a href="#" class="submenu_link">Feminino</a></li>
-                        <li><a href="#" class="submenu_link">Masculino</a></li>
-                        <li><a href="#" class="submenu_link">Unissex</a></li>
-                    </ul>
-                </a></li>
-                <li class="categoria"><a href="" class="categoria_link">
-                    <div class="categoria_titulo">
-                        <span class="nome_titulo">SkinCare</span>
-                        <span class='bx bx-chevron-down'></span>
-                    </div>
-                    <ul class="submenu">
-                        <li><a href="#" class="submenu_link">Limpeza</a></li>
-                        <li><a href="#" class="submenu_link">Esfoliação</a></li>
-                        <li><a href="#" class="submenu_link">Hidratação</a></li>
-                        <li><a href="#" class="submenu_link">Máscara</a></li>
-                        <li><a href="#" class="submenu_link">Protetor Solar</a></li>
-                        <li><a href="#" class="submenu_link">Especiais</a></li>
-                    </ul>
-                </a></li>
-                <li class="categoria"><a href="" class="categoria_link">
-                <div class="categoria_titulo">
-                        <span class="nome_titulo">Cabelo</span>
-                        <span class='bx bx-chevron-down'></span>
-                    </div>
-                    <ul class="submenu">
-                        <li><a href="#" class="submenu_link">Dia-a-dia</a></li>
-                        <li><a href="#" class="submenu_link">Tratamentos</a></li>
-                        <li><a href="#" class="submenu_link">Estilizaçãp</a></li>
-                        <li><a href="#" class="submenu_link">Especiais</a></li>
-                        <li><a href="#" class="submenu_link">Acessórios</a></li>
-                    </ul>
-                </a></li>
-                <li class="categoria"><a href="" class="categoria_link">
-                <div class="categoria_titulo">
-                        <span class="nome_titulo">Eletrônicos</span>
-                        <span class='bx bx-chevron-down'></span>
-                    </div>
-                    <ul class="submenu">
-                        <li><a href="#" class="submenu_link">Cabelo</a></li>
-                        <li><a href="#" class="submenu_link">Pincel</a></li>
-                        <li><a href="#" class="submenu_link">Esponja</a></li>
-                    </ul>
-                </a></li>
-                <li class="categoria"><a href="" class="categoria_link">
-                <div class="categoria_titulo">
-                        <span class="nome_titulo">Corporal</span>
-                        <span class='bx bx-chevron-down'></span>
-                    </div>
-                    <ul class="submenu">
-                        <li><a href="#" class="submenu_link">Body Splash</a></li>
-                        <li><a href="#" class="submenu_link">Óleos</a></li>
-                        <li><a href="#" class="submenu_link">Cremes</a></li>
-                        <li><a href="#" class="submenu_link">Protetores</a></li>
-                    </ul>
-                </a></li>
-            </ul>
-        </div>
-        <div class="botoes_config">
-            <p class="categoria_nome">Ajuda e Configurações</p>
-            <ul>
-                <li class="config"><a href="#" class="config_link">Minha Conta</a></li>
-                <li class="config"><a href="#" class="config_link">Área de Administração</a></li>
-                <li class="config"><a href="" class="config_link">Meus Pedidos</a></li>
-                <li class="config"><a href="" class="config_link">Associados</a></li>
-                <li class="config"><a href="" class="config_link">Termos de Uso e Privacidade</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-    
+
+    <script src="../javascript/SidebarHeader.js"></script>
 </body>
 </html>
