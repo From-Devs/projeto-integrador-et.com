a33d1b3ca7166ed41fd00d121dfb5fcdfc6a1cea
Ryan Gosling <elielmurbach01@gmail.com>
2025-03-07 16:39:04 -0400

    fazendo o header do SIDEBARadm ;)

--- et_pontocom/app/views/tela-de-dados.php
+++ et_pontocom/app/views/tela-de-dados.php
@@ -4,8 +4,3 @@ include "../../public/componentes/sidebar/sidebarADM.php";
 <?php 
 include "../../public/componentes/main.php";
 ?>
-<main>
-    <section>
-        <h1>testes</h1>
-    </section>
-</main>
