8e0e6a43a21cbea1d5e3f991b3e66ba3a9242f78
Ryan Gosling <elielmurbach01@gmail.com>
2025-03-07 16:45:40 -0400

    ç,l,l
    inhjbhubjk

--- et_pontocom/public/componentes/main.php
+++ et_pontocom/public/componentes/main.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../public/css/sidebar/Header.css">
 </head>
 <body>
     <main>
-        <section class="main_header">Meus dados
-        sou o header tem o icone do usuario mais esse é outro componentes que link com a tela so olha no figma
+        <section class="main_header">
+            <h1 class="dados">
+               Meus dados
+            </h1>   
         </section>
         <section class="main_fundo">
-            sou o main_fundo 
-            left tem que por left: 287px; nos dois main_header,main_fundo
-
         </section>
     </main>
 </body>
--- et_pontocom/public/componentes/sidebar/sidebarADM.php
+++ et_pontocom/public/componentes/sidebar/sidebarADM.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/host-gui/projeto/projeto-integrador-et.com/et_pontocom/public/css/sidebar/sidebarA.css">
+    <link rel="stylesheet" href="../../public/css/sidebar/sidebarA.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.4/css/boxicons.min.css">
     
@@ -13,7 +13,7 @@
     <div class="sidebar_adm">
         <nav class="nav_adm">
             <div class="logo">
-                <img src="/host-gui/projeto/projeto-integrador-et.com/et_pontocom/public/imagens/LogoBranca1.png" alt="awdwa">
+                <img src="../../public/imagens/LogoBranca1.png" alt="awdwa">
             </div>
             <div class="linhaGradiente"></div>
             <div class="botoesMenu">
@@ -64,7 +64,7 @@
             </div>
         </nav>
     </div>
-    <script src="/host-gui/projeto/projeto-integrador-et.com/et_pontocom/public/javascript/sidebar/sidebarScript.js"></script>
+    <script src="../../public/javascript/sidebar/sidebarScript.js"></script>
     
 </body>
 </html>
