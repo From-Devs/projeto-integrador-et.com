fa8d943cd9eac9b5cbf43416d60b8009620e9c7b
GuilhermeNantes <guinantes27@gmail.com>
2025-03-10 16:57:25 -0400

    passndo para 01
    sfafsdfsagdqgkedfnçbld lrgkeangjk wlejkrngbjke shklbsjklevrkesblkrgvesbrgjhklsber

--- et_pontocom/public/componentes/main.php
+++ et_pontocom/public/componentes/main.php
@@ -3,17 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/css/sidebar/header.css">
     <title>Document</title>
-    <link rel="stylesheet" href="../../public/css/sidebar/Header.css">
 </head>
 <body>
     <main>
         <section class="main_header">
-            <h1 class="dados">
-               Meus dados
-            </h1>   
+            <h1 id="dados">
+                Meus dados     
+            </h1>
         </section>
         <section class="main_fundo">
+            <div class="quadrado"></div>
         </section>
     </main>
 </body>
