728822d831f00dd1046e5dd2b0dc17c832f94061
Marques Evandro <evandrocastromarques@gmail.com>
2025-03-11 16:20:12 -0400

    adf

--- et_pontocom/app/views/associado/dashboard-associado.php
+++ et_pontocom/app/views/associado/dashboard-associado.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>    
-    <link rel="stylesheet" href="<?php $_SERVER["DOCUMENT_ROOT"];?>et_pontocom/public/css/sidebar/sidebarA.css">
+    <link rel="stylesheet" href="/et_pontocom/public/css/sidebar/sidebarA.css">
 
 </head>
 <body>
     <?php
         require "../../../public/componentes/sidebar/sidebarAssoc.php"
+        
     ?>  
     
 </body>
