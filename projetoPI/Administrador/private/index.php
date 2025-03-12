1b791cdf68537aaa44ed9baf804989b4355f4963
Eduardo Serafim <eduardoserafiim05@gmail.com>
2025-03-12 16:57:50 -0400

    Commit 10
    Tive progresso hoje BASTANTE, preciso fazer as animações do donut e também a corzinha do lado do nome dos top 5 lá, ah e também preciso ajustar o canto arredondado da foto do usuario que tá quadrado ainda

--- projetoPI/Administrador/private/index.php
+++ projetoPI/Administrador/private/index.php
@@ -6,13 +6,47 @@ function botaoPadrao($icone, $nome, $alt=''){
         <div id='iconeClique'>
             <img src='$icone' alt='$alt'>
         </div>
-        <p>$nome</p>
+        <p id='textoBotao'>$nome</p>
     </button>
     ";
 
     echo $html;
 }
 
+function dadosInfor($nomeDiv,$icone, $titulo, $valor, $alt=''){
+    $html = "
+    <div id='$nomeDiv'>
+        <div id='textoInformacao'>
+        <h1 id='textoTituloInform'>$titulo</h1>
+        <h1 id='valorInform'>$valor</h1>
+        </div>
+        <div id='imagemInformaEstilizacao'>
+            <img id='imagemInforma' src='$icone' alt='$alt'>
+        </div>
+    </div>
+    
+    ";
 
+    echo $html;
+}
+
+function pizzas($nomeDiv, $titulo, $pos1='vendedor 1', $pos2='vendedor 2', $pos3='vendedor 3', $pos4='vendedor 4', $pos5='vendedor 5'){
+    $html = "
+    <div id='$nomeDiv'>
+        <h1 id='tituloPizza'>$titulo</h1>
+        <div id='ajusteSelecionador'>
+            <div id='selecionador'>
+                <p id='vendedores'>$pos1</p>
+                <p id='vendedores'>$pos2</p>
+                <p id='vendedores'>$pos3</p>
+                <p id='vendedores'>$pos4</p>
+                <p id='vendedores'>$pos5</p>
+            </div>
+        </div>
+    </div>
+    ";
+
+    echo $html;
+}
 
 ?>
--- projetoPI/Administrador/view/index.php
+++ projetoPI/Administrador/view/index.php
@@ -7,15 +7,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrador</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <div id="containerEsquerda">
         <div id="imagem">
-            <img id="fotoEt" src="./../public/LogoET.png" alt="LogoET">
+            <img id="fotoEt" src="./../public/ET/LogoET.png" alt="LogoET">
         </div>
         <div id="opcoes">
+            <!-- $icone, $nome, $alt='' -->
             <?php botaoPadrao('./../public/icon/dashboard.png','Dashboard','iDash')?>
+            <?php botaoPadrao('./../public/icon/produtos.png','Produtos','iProd')?>
+            <?php botaoPadrao('./../public/icon/pedidos.png','Pedidos','iPedi')?>
+            <?php botaoPadrao('./../public/icon/associados.png','Associados','iAssoc')?>
+            <?php botaoPadrao('./../public/icon/relatorios.png','Relatórios','iRela')?>
+            <?php botaoPadrao('./../public/icon/customizacao.png','Customização','iCustom')?>
+            <div id="saindo">
+                <button id="botaoSair">
+                    <img src="./../public/icon/sair.png" alt="sair">
+                </button>
+                <p id="sairP">Sair</p>
+            </div>  
         </div>
     </div>
+    <div id="container">
+        <div id="controleIcon">
+            <div id="iconUsuario">
+                <img id="fotoUser" src="./../public/userFoto/userIMG.png" alt="userIMG">
+                <p id="textUser">ADM ET</p>
+            </div>
+        </div>
+        <div id="titulo">
+            <h1 id="tituloH1">Dashboard</h1>
+        </div>
+        <div id="geralInformacoes">
+            <!-- $nomeDiv,$icone, $titulo, $valor, $alt=''-->
+            <?php dadosInfor('valorVendas','./../public/iforma/wallet.png','Valor Vendas','R$ 1.500,00','wallet') ?>
+            <?php dadosInfor('lucroLiquido','./../public/iforma/cifrao.png','Lucro Líquido','R$ 569,00','cifrao') ?>
+            <?php dadosInfor('vendas','./../public/iforma/grafico.png','Vendas','233','grafico') ?>
+        </div>
+        <div id="controlePizzas">
+            <div id="divPizzaEsquerda">
+                <?php pizzas('pizzaTop','Top 5 vendedores') ?>
+            </div>
+            <div id="divPizzaDireita">
+                <?php pizzas('Regioes','Regiões que mais compram') ?>
+            </div>
+        </div>
+    </div>
+    <script src="./../private/javascript.js"></script>
 </body>
 </html>
