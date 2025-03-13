46ad1770536dcaf817a3ad0e47246302c0702f46
Nícolas Marcelo Lima Eloy <134300551+nicolasmleloy@users.noreply.github.com>
2025-03-13 17:09:41 -0400

    Estrutura dos detalhes

--- et_pontocom/app/views/usuario/detalhesDoProduto.php
+++ et_pontocom/app/views/usuario/detalhesDoProduto.php
@@ -1,6 +1,6 @@
 <?php
 
-
+require __DIR__ . "/../../../public/componentes/header.php";
 
 ?>
 
@@ -10,9 +10,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalhes do produto</title>
+    <link rel="stylesheet" href="../../../public/css/header.css">
+    <link rel="stylesheet" href="../../../public/css/detalhesDoProduto.css">
+    <link rel="stylesheet" href="../../../public/css/testeSidebar.css">
+    <link rel="stylesheet" href="../../../public/css/teste.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Pixelify+Sans:wght@400..700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Detalhes</h1>
-    <h2>cu<h2>
+    <?php echo createHeader(false)?>
+    
+    <div class="detalhes-principal">
+        <div class="detalhes-imagens">
+            <div class="imagens-lateral">
+                
+            </div>
+            <div class="img-principal">
+
+            </div>
+        </div>
+
+        <div class="detalhes-info">
+            <div>
+                <h2></h2>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="teste">
+    
+    </div>
+    <script src="../../../public/javascript/header.js"></script>
 </body>
 </html>
