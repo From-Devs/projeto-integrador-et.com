086c05410d2f7cf5838002e2bf2bb83fd7a1645a
Ryan Gosling <elielmurbach01@gmail.com>
2025-03-17 14:51:29 -0400

    tentando arrumar essa bomba

--- et_pontocom/public/componentes/main.php
+++ et_pontocom/public/componentes/main.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../public/css/sidebar/header.css">
+    <link rel="stylesheet" href="../../public/css/AdmDados.css">
     <title>Document</title>
 </head>
 <body>
