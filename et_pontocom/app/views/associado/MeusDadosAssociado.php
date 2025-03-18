0548074d9c2d3a3253c85587a22a3a8b3e7aceb2
GuilhermeNantes <guinantes27@gmail.com>
2025-03-18 14:42:57 -0400

    commit main
    kkkkkkkkkkk

--- et_pontocom/app/views/associado/MeusDadosAssociado.php
+++ et_pontocom/app/views/associado/MeusDadosAssociado.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../public/css/MeusDadosAssociado.css">
     <link rel="stylesheet" href="../../../public/componentes/sidebarADM_Associado/style.css">
-    <title>Document</title>
+    <title>Meus Dados</title>
 </head>
 <body>
     <?php 
@@ -13,48 +13,54 @@
     ?>
     <main>
         <section class="main_header">
-            <h1 id="dados">
-                Meus dados     
-            </h1>
-            <div class="perfil">
-                
-           </div>
+            <h1 id="dados">Meus dados</h1>
+            <div class="perfil-box">
+                <div id="quadrado-perfil">
+                    <h1>Perfil</h1>
+                    <img src="https://i.pinimg.com/736x/91/0b/ec/910becbf6e01b541e0dea7f3bcc515c3.jpg" alt="User Profile Image">
+                </div>    
+                <div id="quadrado-perfil"></div>
+
+            </div>
         </section>
         <section class="main_fundo">
-            <div class="quadrado">
+            <div class="pricipais-dados">
+                <div class="editar">
+                    <h1>Editar</h1>
+                </div>
+            </div>
+            <div class="quadrado hidde">
                 <div class="user-img">
-                        <h1>Perfil</h1>
-                        <img src="https://i.pinimg.com/736x/91/0b/ec/910becbf6e01b541e0dea7f3bcc515c3.jpg" alt="">
+                    <h1>Perfil</h1>
+                    <img src="https://i.pinimg.com/736x/91/0b/ec/910becbf6e01b541e0dea7f3bcc515c3.jpg" alt="User Profile Image">
+                </div>
+                <div class="user-dados">
+                    <div class="user-Letra">
+                        <h1>Nome</h1>
+                        <div class="user_Box menor Texto">Wellinton R.</div>
                     </div>
-                    <div class="user-dados">
-                        <div class="user-Letra">    
-                            <h1>Nome</h1>
-                        </div>
-                            <div class="user_Box menor Texto">Wellinton R.</div>
-                        <div class="user-Letra">
-                            <h1>Gmail</h1>
-                        </div>
-                            <div class="user_Box Texto">tiginhoJogoTÀPagandoMUITO@Gmail.com</div>
-                        <div class="user-Letra">
-                            <h1>CPF</h1>
-                        </div>
-                            <div class="user_Box menor Texto">09x.xxx,xxx-67</div>
-                        <div class="user-Letra">
-                            <h1>Adicionar</h1>
-                        </div>
+                    <div class="user-Letra">
+                        <h1>Gmail</h1>
+                        <div class="user_Box Texto">tiginhoJogoTÀPagandoMUITO@Gmail.com</div>
+                    </div>
+                    <div class="user-Letra">
+                        <h1>CPF</h1>
+                        <div class="user_Box menor Texto">09x.xxx,xxx-67</div>
+                    </div>
+                    <div class="user-Letra">
+                        <h1>Adicionar</h1>
                         <div class="icones">
                             <i class='bx bxl-instagram icone'></i>
                             <i class='bx bxl-linkedin icone'></i>
-                            <i class='bx bxl-facebook icone' ></i>
-                            <i class='bx bxl-twitter icone' ></i>
-                            <i class='bx bxl-whatsapp icone' ></i>
+                            <i class='bx bxl-facebook icone'></i>
+                            <i class='bx bxl-twitter icone'></i>
+                            <i class='bx bxl-whatsapp icone'></i>
                             <i class='bx bx-plus icone'></i>
                         </div>
                     </div>
-                    <i class='bx bx-x exit'></i>
                 </div>
+                <i class='bx bx-x exit'></i>
             </div>
-
         </section>
     </main>
     <script src="../../../public/componentes/sidebarADM_Associado/script.js"></script>
