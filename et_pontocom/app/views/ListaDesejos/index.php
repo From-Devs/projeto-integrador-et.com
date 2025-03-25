99c252207f96228132f8241d08cab8b804112c9a
DanielPydev <danielprosperodefigueiredo@gmail.com>
2025-03-25 14:04:38 -0400

    commit geral

--- et_pontocom/app/views/ListaDesejos/index.php
+++ et_pontocom/app/views/ListaDesejos/index.php
@@ -4,218 +4,123 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
+    <div class="title-container">
+        <div class="title">
+            <h1>MINHA LISTA DE DESEJOS</h1>
+        </div>
+        <center><div class="line"></div></center>
+    </div>
+
     <div class="container">
-        <!--<div class="cabec">
-            <div class="menu">
-                <img src="./img/Frame 211.png" alt="">
-            </div>
-
-            <div class="logo">
-                <img src="./img/imagem_2024-07-01_161442210 4.png" alt="">
-            </div>
-
-            <div class="outros">
-                <div class="lupa">
-                    <img src="./img/basil_search-solid.png" alt="" >
-                </div>
-
-                <div class="car">
-                    <img src="./img/Group 192.png" alt="" >
-                </div>
-
-                <div class="perfil">
-                    <img src="./img/iconamoon_profile-circle-light.png" alt="" >
-                </div>
-            </div>
-        </div>-->
-
-        <div class="titleListDesejos">
-            <h3>MINHA LISTA DE DESEJO</h3>
-        </div>
-        
-        <div class="Line01">
-            <div class="line01"></div>
-        </div>
-
-        <div class="infoCima">
-            <div class="prod">
-                <h4>Produto</h4>
-            </div>
-
-            <div class="preco">
-                <h4>Preço</h4>
-            </div>
-        </div>
-
-        <div class="Card01">
+        <div class="card-container">
             <div class="card01">
                 <div class="cardImg01">
-                    <img src="./img/creme_black.png" alt="">
+                    <img src="./img/prod01.png" alt="">
                 </div>
 
-                <div class="precoCard01">
-                    <p>55,90R$</p>
+                <div class="cardPreco01">
+                    <p><strong>R$50,00</strong></p>
                 </div>
 
-                <div class="descrCard01">
+                <div class="cardInfo01">
                     <p>Nivea  HIDRATANTE CORPORAL MILK</p>
                 </div>
 
-                <div class="dateCard">
-                    <p><strong>Adicionado 20/08/2024</strong></p>
+                <div class="cardDate01">
+                    <p><strong>Adicionado 11/03/25</strong></p>
                 </div>
 
-                <div class="buttom01">
+                <div class="cardCheck01">
                     <input type="checkbox">
                 </div>
             </div>
-        </div>
 
-        <div class="Card02">
             <div class="card02">
                 <div class="cardImg02">
-                    <img src="./img/cremeColo.png" alt="">
+                    <img src="./img/prod02.png" alt="">
                 </div>
 
-                <div class="precoCard02">
-                    <p>55,90R$</p>
+                <div class="cardPreco02">
+                    <p><strong>R$50,00</strong></p>
                 </div>
 
-                <div class="descrCard02">
+                <div class="cardInfo02">
                     <p>Nivea  HIDRATANTE CORPORAL MILK</p>
                 </div>
 
-                <div class="dateCard">
-                    <p><strong>Adicionado 20/08/2024</strong></p>
+                <div class="cardDate02">
+                    <p><strong>Adicionado 11/03/25</strong></p>
                 </div>
 
-                <div class="buttom02">
+                <div class="cardCheck02">
                     <input type="checkbox">
                 </div>
             </div>
-        </div>
 
-        <div class="Card03">
             <div class="card03">
                 <div class="cardImg03">
-                    <img src="./img/coffe.png" alt="">
+                    <img src="./img/prod03.png" alt="">
                 </div>
 
-                <div class="precoCard03">
-                    <p>55,90R$</p>
+                <div class="cardPreco03">
+                    <p><strong>R$50,00</strong></p>
                 </div>
 
-                <div class="descrCard03">
+                <div class="cardInfo03">
                     <p>Nivea  HIDRATANTE CORPORAL MILK</p>
                 </div>
 
-                <div class="dateCard">
-                    <p><strong>Adicionado 20/08/2024</strong></p>
+                <div class="cardDate03">
+                    <p><strong>Adicionado 11/03/25</strong></p>
                 </div>
 
-                <div class="buttom03">
+                <div class="cardCheck03">
                     <input type="checkbox">
                 </div>
             </div>
-        </div>
 
-        <div class="Card04">
             <div class="card04">
                 <div class="cardImg04">
-                    <img src="./img/nivea.png" alt="">
+                    <img src="./img/prod04.png" alt="">
                 </div>
 
-                <div class="precoCard04">
-                    <p>55,90R$</p>
+                <div class="cardPreco04">
+                    <p><strong>R$50,00</strong></p>
                 </div>
 
-                <div class="descrCard04">
+                <div class="cardInfo04">
                     <p>Nivea  HIDRATANTE CORPORAL MILK</p>
                 </div>
 
-                <div class="dateCard">
-                    <p><strong>Adicionado 20/08/2024</strong></p>
+                <div class="cardDate04">
+                    <p><strong>Adicionado 11/03/25</strong></p>
                 </div>
 
-                <div class="buttom04">
+                <div class="cardCheck04">
                     <input type="checkbox">
                 </div>
             </div>
-        </div>
-
-        <div class="Card05">
-            <div class="card05">
-                <div class="cardImg05">
-                    <img src="./img/avon-removebg-preview 1.png" alt="">
-                </div>
-
-                <div class="precoCard05">
-                    <p>55,90R$</p>
-                </div>
-
-                <div class="descrCard05">
-                    <p>Nivea  HIDRATANTE CORPORAL MILK</p>
-                </div>
-
-                <div class="dateCard">
-                    <p><strong>Adicionado 20/08/2024</strong></p>
-                </div>
-
-                <div class="buttom05">
-                    <input type="checkbox">
-                </div>
-            </div>
-        </div>
-
-        <div class="Card06">
-            <div class="card06">
-                <div class="cardImg06">
-                    <img src="./img/135296-removebg-preview 1.png" alt="">
-                </div>
-
-                <div class="precoCard06">
-                    <p>55,90R$</p>
-                </div>
-
-                <div class="descrCard06">
-                    <p>Nivea  HIDRATANTE CORPORAL MILK</p>
-                </div>
-
-                <div class="dateCard">
-                    <p><strong>Adicionado 20/08/2024</strong></p>
-                </div>
-
-                <div class="buttom06">
-                    <input type="checkbox">
-                </div>
-            </div>
-        </div>
-
-        <div class="Line02">
-            <div class="line02"></div>
-        </div>
-
-        <div class="Buttons_add_delete">
-            <div class="button01Add">
-                <div class="button01_add">
-                    <button type="submit"><img src="./img/Group 49.png" alt=""> <p>Adicionar ao carrinho</p> </button>
-                </div>
-            </div>
-    
-            <div class="button01Excluir">
-                <div class="button01_excluir">
-                    <button type="submit">Excluir</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="CabeExemplo">
-
         </div>
     </div>
+
+    <div class="button-container">
+        <div class="button01">
+            <button type="submit"><img src="./img/imgButton.png" alt=""><p>Adicionar ao carrinho</p></button>
+        </div>
+
+        <div class="button02">
+            <button type="submit"><p>Excluir</p></button>
+        </div>
+    </div>
+
+    <center><div class="line2"></div></center>
+    
+    
 </body>
 </html>
---
+++ et_pontocom/app/views/SobreAssoci/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>Sobre Associados</title>
+</head>
+<body>
+    <div class="container">
+        <div class="initImg">
+            <img src="./img/imgInicial.png" alt="">
+        </div>
+
+        <div class="initTitle">
+            <div class="cont-title">
+                <div class="titleIni">
+                    <h1>ASSOCIADOS</h1>
+                </div>
+            </div>
+
+            <center><div class="linha01"></div></center>
+        </div>
+
+        <div class="cont-text">
+            <div class="text01">
+                <h3>ASSOCIADOS ET.COM</h3>
+                <p>Seja bem-vindo ao programa de marketing de afiliados da nossa empresa ET.COM. O Programa de Associados. Com milhões de produtos e diversos programas disponíveis no ET.COM, os associados podem usar ferramentas simples de criação de links para direcionar o público às suas recomendações e faturar com compras e programas qualificados.</p>
+            </div>
+
+            <div class="text02">
+                <h3>COMO FUNCIONA O PROGRAMA DE ASSOCIADOS?</h3>
+                <p> O Programa de acossiados permitiria que a pessoa compartilhasse links, e a realização de divulgação em suas redes sociais, blog, site, canal no YouTube ou e-mails. Cada vez que o associado realizasse uma venda por meio desse link, ele receberia uma comissão previamente definida pelo produtor.</p>
+            </div>
+        </div>
+
+
+        <div class="card">
+            <div class="card-box">
+                <img src="./img/imgCard01.png" alt="">
+                <button>Associar-se</button>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <!--<div class="cardContent">
+        <div class="cardBackground">
+            <img class="backgroundImg" src="./img/imgAssoc.png" alt="">
+            <div class="informacao">
+                <div class="cardLogo">
+                        <img src="./img/Logo01.png" alt="">
+                    </div>
+
+                    <div class="cardInfo">
+                        <p>DIVULGUE NOSSOS PRODUTOS, E RECEBA COMISSÕES</p>
+                    </div>
+                    <div class="cardLine">
+                        <div class="line"></div>
+                    </div>
+            </div>
+        </div>
+        
+        <button class="btnAssoc">Associar-se</button>
+</div>-->
+
+    <!-- <div class="cont-card">
+        <div class="card">
+            <div class="contBlur">
+                <div class="cardBackground">
+                    <img src="./img/imgAssoc.png" alt="">
+                </div>
+
+                <div class="cardLogo">
+                    <img src="./img/Logo01.png" alt="">
+                </div>
+
+                <div class="cardInfo">
+                    <p>DIVULGUE NOSSOS PRODUTOS, E RECEBA COMISSÕES</p>
+                </div>
+
+                <div class="cardLine">
+                    <div class="line"></div>
+                </div>
+            </div>
+
+            <div class="cardButton">
+                <button>Associar-se</button>
+            </div>
+        </div>
+    </div> -->
+
+
+    
+</body>
+</html>
