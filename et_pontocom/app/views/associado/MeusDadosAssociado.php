3bfb0d3cc39a57969abab1ea2d9f4cd506f3ac8d
Ryan Gosling <elielmurbach01@gmail.com>
2025-03-25 16:43:37 -0400

    juntando main

--- et_pontocom/app/views/associado/MeusDadosAssociado.php
+++ et_pontocom/app/views/associado/MeusDadosAssociado.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../public/css/MeusDadosAssociado.css">
-    <link rel="stylesheet" href="../../../public/componentes/sidebarADM_Associado/style.css">
+    <link rel="stylesheet" href="../../../public/componentes/sidebarADM_Associado/styles.css">
     <title>Meus Dados</title>
 </head>
 <body>
