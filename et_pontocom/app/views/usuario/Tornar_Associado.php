027e37977f9cd8ba88e06a4c2cbcca8f8643d4d6
Vini909 <viniciuselionai18@gmail.com>
2025-03-25 16:45:41 -0400

    Vinicius TonarAssociado
    Primeira versão de como se tornar um associado

---
+++ et_pontocom/app/views/usuario/Tornar_Associado.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Tornar_Associado.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+
+</head>
+<body>
+    <header class="teste">
+        <div class="logo">Logo</div>
+        <div class="icons">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </header>
+    <section class="promo-section">
+        <div class="promo-content">
+            <h2>Torne-se um(a) associado(a) ET!</h2>
+            <p>Seja bem-vindo ao programa de marketing de associados da nossa empresa ETCOM. O Programa de Associados ETCOM ajuda vendedores, editores e blogueiros a monetizarem seus sites.</p>
+            <a href="#" class="join-now">Associar-se</a>
+        </div>
+        <div class="promo-image">
+            <img src="moça.png" alt="Promo Image">
+        </div>
+    </section>
+    <div class="carousel-container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="2.png" class="d-block w-100" alt="Marcos">
+                    <div class="name">Marcos</div>
+                </div>
+                <div class="carousel-item">
+                    <img src="TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Jose">
+                    <div class="name">Jose</div>
+                </div>
+                <div class="carousel-item">
+                    <img src="TornarAssociado/manoel-gomes (1) 4.png" class="d-block w-100" alt="Manoel">
+                    <div class="name">Manoel</div>
+                </div>
+                <div class="carousel-item">
+                    <img src="TornarAssociado/DeWatermark.ai_1721330090347 (1) 2.png" class="d-block w-100" alt="Karla">
+                    <div class="name">Karla</div>
+                </div>
+                <div class="carousel-item">
+                    <img src="TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Lucas">
+                    <div class="name">Lucas</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="commission-table">
+        <h2>Comissões</h2>
+        <p>A Partir de 7%</p>
+        <p>Seja bem-vindo ao programa de marketing de afiliados da nossa empresa ETCOM! O Programa de Associados ETCOM ajuda criadores de conteúdo, editores e blogueiros a monetizarem seus sites com links e banners publicitários e diversos programas disponíveis no ETCOM, os associados podem usar nossas ferramentas exclusivas, linhas para desenvolver campanhas publicitárias e suas recomendações futuras em compras e programas qualificados.</p>
+    </section>
+    <section class="cards-section">
+        <div class="cards-container">
+            <div class="card card1">
+                <img src="TornarAssociado/card1.png" alt="Descrição da Imagem do Card">
+                <div class="card-content">
+                </div>
+            </div>
+            <div class="card card2">
+                <img src="TornarAssociado/card2.png" alt="Descrição da Imagem do Card">
+                <div class="card-content">
+                </div>
+            </div>
+            <div class="card card3">
+                <img src="TornarAssociado/card3.png" alt="Descrição da Imagem do Card">
+                <div class="card-content">
+                </div>
+            </div>
+            <div class="card card4">
+                <img src="TornarAssociado/card4.png" alt="Descrição da Imagem do Card">
+                <div class="card-content">
+                </div>
+            </div>
+            <div class="card card5">
+                <img src="TornarAssociado/card5.png" alt="Descrição da Imagem do Card">
+                <div class="card-content">
+                </div>
+            </div>
+        </div>
+            <div class="caixa_salmão">
+                <img src="TornarAssociado/Group 1000003753.png" alt="img">
+            </div>
+    <section class="related-products">
+        <h2>Relacionados</h2>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <footer>
+        <div class="footer-links">
+            <a href="#">Sobre</a>
+            <a href="#">Contato</a>
+            <a href="#">Política de Privacidade</a>
+        </div>
