ac86d1c06a1afc4c870462dd55e561650900ab7a
Nicolle Zaleski <nicolle.m.zaleski@gmail.com>
2025-03-27 14:42:50 -0400

    alterando ordem de css

--- et_pontocom/public/componentes/sidebarADM_Associado/sidebarADM.php
+++ et_pontocom/public/componentes/sidebarADM_Associado/sidebarADM.php
@@ -11,9 +11,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../sidebarADM_Associado/styles.css">
-    <link rel="stylesheet" href="../botao/botoesComponente.css">
-    <link rel="stylesheet" href="../popUp/popUpComponente.css">
+    <link rel="stylesheet" href="../botao/styles.css">
+    <link rel="stylesheet" href="../popUp/styles.css">
+    <link rel="stylesheet" href="./styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.4/css/boxicons.min.css">
     
