90c762333edea2ad229d513d61cc888afae3e6c2
Robert Fernandes <robertbetinho.filho@gmail.com>
2025-04-15 14:35:52 -0400

    atualizandp

--- et_pontocom/public/componentes/loginassociado/InputsLoginAssoc.php
+++
@@ -1,8 +0,0 @@
-<?php
-function Campos($nome,$tipo="text",$class="campo"){
-    return "<div class=\"organizacao\">
-            <input type=\"$tipo\" name=\"$nome\" class=\"$class\" required placeholder=\"\">
-             <label for=\"$nome\">$nome</label>
-            </div>";
-}
-?>
