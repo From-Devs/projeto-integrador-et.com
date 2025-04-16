3c97cc20ef5cdb7cedab9ba619e5b6b78f629587
Eduardo Serafim <eduardoserafiim05@gmail.com>
2025-04-16 17:17:21 -0400

    Componentes Tabela
    comecei trocando os caminhos na dashboard das imagens dos dados Informação que estavam errados, agora ta tudo certo. Fiz a tabela do historico de vendas do associado que também ta pronto, produto do ADM tá pronto a tabela no caso, no momento só to fazendo a componentização das tabelas. iniciei a fazer o produto do associado, mas falta o botão de excluir e editar na tabela. OBS: eu deixei o iconUsuario agora com margin-top: 20px; pois a tabela de produtos não estava conseguindo ter um margin em baixo legal, pois ficava "colado" na parte inferior da pagina.  16/04

--- et_pontocom/app/views/adm/Associados.php
+++ et_pontocom/app/views/adm/Associados.php
@@ -1,6 +1,7 @@
 <?php 
 
 include __DIR__ . "/../../../public/componentes/telaADM/componenteADM.php";
+include __DIR__ . "/../../../public/componentes/tabelasAssociado_ADM/AssociadosADM/associados.php";
 require_once __DIR__ . "/../../../public/componentes/sidebarADM_Associado/sidebarInterno.php";
 require_once __DIR__ . "/../../../public/componentes/popUp/popUp.php";
 require_once __DIR__ . "/../../../public/componentes/botao/botao.php";
@@ -47,64 +48,9 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? 'ADM';
         <div id="titulo">
             <h1 id="tituloH1">Associados</h1>
         </div>
-        <div id="lista">
-            <table id="tabelaVendas">
-                <thead id="barraCima">
-                    <tr>
-                        <th id="bordaEsquerda" scope='col'>#</th>
-                        <th id="th2" scope='col'>Produto</th>
-                        <th id="th3" scope='col'>Associado</th>
-                        <th id="th4" scope='col'>SKU</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Hidratante</td>
-                        <td>Associado</td>
-                        <td>S5D56GE</td>
-                        <!-- provavelmente vai ter que ter um botão aqui -->
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Base Líquida</td>
-                        <td>Associado</td>
-                        <td>FA9DSF56</td>
-                        <!-- provavelmente vai ter que ter um botão aqui -->
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Body Splash</td>
-                        <td>Associado</td>
-                        <td>UJ47R8S</td>
-                        <!-- provavelmente vai ter que ter um botão aqui -->
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>Colônia Coffe Woman</td>
-                        <td>Associado</td>
-                        <td>FDAS94A</td>
-                        <!-- provavelmente vai ter que ter um botão aqui -->
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>Skincare</td>
-                        <td>Associado</td>
-                        <td>9WE8FWS</td>
-                        <!-- provavelmente vai ter que ter um botão aqui -->
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>Césio Liquído</td>
-                        <td>Associado</td>
-                        <td>F99W2C9</td>
-                        <!-- provavelmente vai ter que ter um botão aqui -->
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
+        <?php 
+            echo associadosTabela()
+        ?>
     <script src="/projeto-integrador-et.com/et_pontocom/public/componentes/sidebarADM_Associado/scripts.js"></script>
     <script src="/projeto-integrador-et.com/et_pontocom/public/componentes/popup/script.js"></script>
 </body>
