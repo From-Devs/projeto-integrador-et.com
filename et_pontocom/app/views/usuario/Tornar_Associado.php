169222327e2c398ca8cfb6c72892b3000f830b3b
Vini909 <viniciuselionai18@gmail.com>
2025-04-22 16:42:04 -0400

    imagens,css e php
    ajustando o css php e as imagens mudei caminhos e e adicionei mais imagens

--- et_pontocom/app/views/usuario/Tornar_Associado.php
+++ et_pontocom/app/views/usuario/Tornar_Associado.php
@@ -1,6 +1,7 @@
 <?php
     require __DIR__ . "/../../../public/componentes/header/header.php"; // import do header
     require __DIR__ . "/../../../public/componentes/rodape/Rodape.php";
+    require __DIR__ . "/../../../public/componentes/botao/botao.php";
     session_start();
     // $tipo_usuario = $_SESSION['tipo_usuario'] ?? 'Cliente';
     $tipoUsuario = $_SESSION['tipo_usuario'] ?? "Associado";
@@ -11,8 +12,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Tornar_Associado.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/css/Tornar_Associado.css">
+    <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/rodape/styles.css">
+    <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/header/styles.css">
+    <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/botao/styles.css">
+    <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/sidebar/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <title>Document</title>
 
@@ -29,9 +33,17 @@
     <a href="#" class="join-now">Associar-se</a>
   </div>
   <div>
-    <img  class="promo-image" src="TornarAssociado/moça.png" alt="Promo Image">
+    <img  class="promo-image" src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/moça.png" alt="Promo Image">
   </div>
-  <?php include 'onda.php'; ?>
+<?php
+    function createOnda($tipo){
+        if ($tipo == 1){
+            return "<div class='wave solida'></div>";
+        }else{
+            return "<div class='wave fantasma'></div>";
+        }
+    }
+?>
     <div>
         <?php
         echo createOnda(1); // Adiciona a onda sólida
@@ -39,7 +51,7 @@
     </div>
 
 </section>
-    <img src="TornarAssociado/karla.nome.png" alt="Texto substituído" class="substituir-imagem">
+    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/karla.nome.png" alt="Texto substituído" class="substituir-imagem">
     <div class="carousel-container">
     <section class="associ">
         <h2>Associados:</h2>
@@ -51,19 +63,19 @@
                     <div class="name">Marcos</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Jose">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Jose">
                     <div class="name">Jose</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/manoel-gomes (1) 4.png" class="d-block w-100" alt="Manoel">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens//projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/manoel-gomes (1) 4.png" class="d-block w-100" alt="Manoel">
                     <div class="name">Manoel</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/DeWatermark.ai_1721330090347 (1) 2.png" class="d-block w-100" alt="Karla">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/DeWatermark.ai_1721330090347 (1) 2.png" class="d-block w-100" alt="Karla">
                     <div class="name">Karla</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Lucas">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Lucas">
                     <div class="name">Lucas</div>
                 </div>
                 <div class="carousel-item active">
@@ -71,19 +83,19 @@
                     <div class="name">Marcos</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Jose">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Jose">
                     <div class="name">Jose</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/manoel-gomes (1) 4.png" class="d-block w-100" alt="Manoel">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/manoel-gomes (1) 4.png" class="d-block w-100" alt="Manoel">
                     <div class="name">Manoel</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/DeWatermark.ai_1721330090347 (1) 2.png" class="d-block w-100" alt="Karla">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/DeWatermark.ai_1721330090347 (1) 2.png" class="d-block w-100" alt="Karla">
                     <div class="name">Karla</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Lucas">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Lucas">
                     <div class="name">Lucas</div>
                 </div>
                 <div class="carousel-item active">
@@ -91,19 +103,19 @@
                     <div class="name">Marcos</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Jose">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Jose">
                     <div class="name">Jose</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/manoel-gomes (1) 4.png" class="d-block w-100" alt="Manoel">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/manoel-gomes (1) 4.png" class="d-block w-100" alt="Manoel">
                     <div class="name">Manoel</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/DeWatermark.ai_1721330090347 (1) 2.png" class="d-block w-100" alt="Karla">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/DeWatermark.ai_1721330090347 (1) 2.png" class="d-block w-100" alt="Karla">
                     <div class="name">Karla</div>
                 </div>
                 <div class="carousel-item">
-                    <img src="TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Lucas">
+                    <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/png-transparent-man-standing-while-wearing-blue-dress-shirt-man-man-people-candle-canon-thumbnail (2) 2.png" class="d-block w-100" alt="Lucas">
                     <div class="name">Lucas</div>
                 </div>
             </div>
@@ -116,34 +128,34 @@
     <section class="cards-section">
         <div class="cards-container">
             <div class="card card1">
-                <img src="TornarAssociado/card1.png" alt="Descrição da Imagem do Card">
+                <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/card1.png" alt="Descrição da Imagem do Card">
                 <div class="card-content">
                 </div>
             </div>
             <div class="card card2">
-                <img src="TornarAssociado/card2.png" alt="Descrição da Imagem do Card">
+                <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/card2.png" alt="Descrição da Imagem do Card">
                 <div class="card-content">
                 </div>
             </div>
             <div class="card card3">
-                <img src="TornarAssociado/card3.png" alt="Descrição da Imagem do Card">
+                <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/card3.png" alt="Descrição da Imagem do Card">
                 <div class="card-content">
                 </div>
             </div>
             <div class="card card4">
-                <img src="TornarAssociado/card4.png" alt="Descrição da Imagem do Card">
+                <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/card4.png" alt="Descrição da Imagem do Card">
                 <div class="card-content">
                 </div>
             </div>
             <div class="card card5">
-                <img src="TornarAssociado/card5.png" alt="Descrição da Imagem do Card">
+                <img src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/card5.png" alt="Descrição da Imagem do Card">
                 <div class="card-content">
                 </div>
             </div>
         </div>
             <div>
-                <img class="sej" src="TornarAssociado/sej.png" alt="img">
-                <img class="caixa_salmão" src="TornarAssociado/Group 1000003753.png" usemap="#image-map">
+                <img class="sej" src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/sej.png" alt="img">
+                <img class="caixa_salmão" src="/projeto-integrador-et.com/et_pontocom/public/imagens/TornarAssociado/Group 1000003753.png" usemap="#image-map">
                 <map name="image-map">
                     <area target="" alt="" title="" href="" coords="1206,339,936,234" shape="rect">
                 </map>
