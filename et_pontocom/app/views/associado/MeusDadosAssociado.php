99e3527790db693b8203aebf89c7b2a19f171616
GuilhermeNantes <guinantes27@gmail.com>
2025-05-07 17:15:33 -0400

    fiz us_box_1
    sdfsdfsdfsdf

--- et_pontocom/app/views/associado/MeusDadosAssociado.php
+++ et_pontocom/app/views/associado/MeusDadosAssociado.php
@@ -8,34 +8,44 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? 'Associado';
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meus Dados</title>
+
+    <!-- Estilos -->
     <link rel="stylesheet" href="../../../public/css/MeusDadosAssociado.css">
     <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/botao/styles.css">
     <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/popUp/styles.css">
     <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/sidebarADM_Associado/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.4/css/boxicons.min.css">
-    <title>Meus Dados</title>
 </head>
 <body>
-    <?php
-        echo createSidebarInterna($tipo_usuario);
-    ?>
-    <div class="seu_pai">
+    <?= createSidebarInterna($tipo_usuario); ?>
+
+    <div class="container-principal">
         <header>
             <div class="titulo">Meus dados</div>
         </header>
-        <div class="fundo_1">
-            <div class="box">
-                <div class="box_us_1"></div>
-                <div class="box_ds_1"></div>
-                <div class="box_d_1"></div>
-                <div class="box_us_p2"></div>
-            </div>
 
+        <div class="area-conteudo">
+            <div class="box-principal">
+                <div class="usuario-bloco-1">
+                    <div class="usuario_titulo">
+                        <h1>Meus dados</h1>
+                    </div>
+                    <div class="usuario_imagem_1">
+                        <img src="https://uploads.spiritfanfiction.com/historias/capas/202201/ben-10-com-alcool-23539545-270120222055.jpg" alt="">
+                        <p>nome sobrenome</p>
+                    </div>
+                    <div class="x">X</div>
+                </div>
+                <div class="dados-secundarios"></div>
+                <div class="dados-topo"></div>
+                <div class="usuario-bloco-2"></div>
+            </div>
         </div>
     </div>
 </body>
