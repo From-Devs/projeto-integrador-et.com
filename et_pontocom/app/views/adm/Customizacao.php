57eaf2a15ff08e37e1be18d16b146139042534f8
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-05-13 23:24:02 -0400

    começando slider

--- et_pontocom/app/views/adm/Customizacao.php
+++ et_pontocom/app/views/adm/Customizacao.php
@@ -4,6 +4,7 @@ include __DIR__ . "/../../../public/componentes/telaADM/componenteADM.php";
 require_once __DIR__ . "/../../../public/componentes/sidebarADM_Associado/sidebarInterno.php";
 require_once __DIR__ . "/../../../public/componentes/popUp/popUp.php";
 require_once __DIR__ . "/../../../public/componentes/botao/botao.php";
+require __DIR__ . "/../../../public/componentes/cardLancamento/produtoLancamento.php";
 
 session_start();
 $tipo_usuario = $_SESSION['tipo_usuario'] ?? 'ADM';
@@ -16,11 +17,13 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? 'ADM';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrador - Customização</title>
     
-    <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/css/CustomizacaoADM.css">
-
+    
     <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/botao/styles.css">
     <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/popUp/styles.css">
     <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/sidebarADM_Associado/style.css">
+    <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/componentes/cardLancamento/styles.css">
+    <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/css/sliderProdutos.css">
+    <link rel="stylesheet" href="/projeto-integrador-et.com/et_pontocom/public/css/CustomizacaoADM.css">
 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
@@ -59,15 +62,41 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? 'ADM';
     
                 <?php echo botaoPersonalizadoOnClick("Atualizar","btn-white", "", "220px", "45px", "20px")?>
 
-                <ul>
+                <ul class="descricaoContainer">
                     <li class="descricao">Clique em um produto para editar</li>
                     <li class="descricao">Arraste os produtos para organizar a ordem de apresentação</li>
                 </ul>
             </div>
+
+            <div class="sessaoLancamento">
+                <ul>
+                    <li class="tituloSessao">Lançamentos</li>
+                </ul>
+                <div class="teste">
+                    <div class="frameSlider">
+                        <i class="setaSlider setaEsquerda" id="esquerda"></i>
+                        <div class="degradeEsquerda"></div>
+                        <div class="frameProdutos">
+                            <div class="containerProdutos" id="containerLancamentos">
+                                <?php
+                                echo createCardProdutoLancamento("Phállebeauty", "Base Matte Alta Cobertura","R$ 1000,00","#E1B48C","matte.jpg");
+                                echo createCardProdutoLancamento("Avon", "Red Batom","R$ 2000,00","#D1061D","batom.png");
+                                echo createCardProdutoLancamento("Benefit", "BADgal Bang! Máscara de Cílios","R$ 3000,00","#D02369","bang.png");
+                                echo createCardProdutoLancamento("Avon", "Color Trend Delineador Líquido","R$ 1000,00","#F0CBDA","trend.webp");
+                                echo createCardProdutoLancamento("Mari Maria","Diamond Blender Esponja de Maquiagem","R$ 2000,00","#D79185","tri.jpeg");
+                                echo createCardProdutoLancamento("Simple Organic", "SOLUÇÃO RETINOL-LIKE","R$ 3000,00","#C9A176","simple.webp");
+                                echo createCardProdutoLancamento("Princess","Mini Chapinha Bivolt","R$ 2000,00","#745CA3","chapa.webp");
+                                echo createCardProdutoLancamento("O Boticário","L'eau De Lily Soleil Perfume Feminino","R$ 3000,00","#F4C83C","lily.jpg");
+                                ?>
+                            </div>
+                        </div>
+                        <div class="degradeDireita"></div>
+                        <i class="setaSlider setaDireita" id="direita"></i>
+                    </div>
+
+                </div>
+            </div>
             
-            <ul>
-                <li class="tituloSessao">Lançamentos</li>
-            </ul>
             <ul>
                 <li class="tituloSessao">Produto em destaque</li>
             </ul>
@@ -80,5 +109,6 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? 'ADM';
     <script src="./../../../public/javascript/javascriptADM.js"></script>
     <script src="/projeto-integrador-et.com/et_pontocom/public/componentes/sidebarADM_Associado/scripts.js"></script>
     <script src="/projeto-integrador-et.com/et_pontocom/public/componentes/popup/script.js"></script>
+    <script src="/projeto-integrador-et.com/et_pontocom/public/javascript/slider.js"></script>
 </body>
 </html> 
