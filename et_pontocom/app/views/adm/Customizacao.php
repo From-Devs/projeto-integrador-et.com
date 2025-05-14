f4b1bae55f72a645c59efa77faca42849dcb72f5
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-05-14 14:21:08 -0400

    Update Customizacao.php

--- et_pontocom/app/views/adm/Customizacao.php
+++ et_pontocom/app/views/adm/Customizacao.php
@@ -28,6 +28,16 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? 'ADM';
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.4/css/boxicons.min.css">
+
+    <style>
+        .degradeEsquerda{
+            background-image: linear-gradient(to right, #F8F8F8 0%, #F8F8F8 35%, rgba(255, 255, 255, 0) 100%);
+        }
+
+        .degradeDireita{
+            background-image: linear-gradient(to left, #F8F8F8 0%, #F8F8F8 35%, rgba(255, 255, 255, 0) 100%);
+        }
+    </style>
 </head>
 <body>
 
