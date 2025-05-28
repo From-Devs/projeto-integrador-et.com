46b756f7c2e139b64d42b94f79e1051078ae008f
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-05-28 17:04:21 -0400

    fiz a animação dos produtos lançamentos

--- et_pontocom/public/javascript/customizacaoADM.js
+++ et_pontocom/public/javascript/customizacaoADM.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener("DOMContentLoaded", function(){
 
     const cardProdutos = document.querySelectorAll('.cardLancamento');
@@ -10,13 +12,25 @@ document.addEventListener("DOMContentLoaded", function(){
         overlayHover.classList.add("overlayHover")
 
         const botaoOverlay = document.createElement('button')
-        botaoOverlay.className = "btn btn-white";
+        botaoOverlay.className = "btn btn-white botaoOverlay";
         botaoOverlay.innerHTML = "Editar";
 
         overlayHover.appendChild(botaoOverlay);
-
         card.append(overlayHover);
 
+        card.addEventListener("mouseenter", function(){
+            overlayHover.style.opacity = "1";
+        })
+        card.addEventListener("mouseleave", function(){
+            overlayHover.style.opacity = "0";
+        })
+
+    })
+
+    const allBotaoOverlay = document.querySelectorAll('.botaoOverlay');
+
+    allBotaoOverlay.forEach(function(botao){
+        botao.addEventListener("click", abrirPopUp("popUpSelectProduto"))
     })
 
 });
