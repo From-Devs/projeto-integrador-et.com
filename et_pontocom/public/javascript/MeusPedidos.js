b1d23eed85f474265d8a708ad2be999b9ac7b924
Nicolle Zaleski <nicolle.m.zaleski@gmail.com>
2025-06-02 17:08:47 -0400

    js e css do Meus Pedidos

--- et_pontocom/public/javascript/MeusPedidos.js
+++ et_pontocom/public/javascript/MeusPedidos.js
@@ -23,11 +23,51 @@ fetch("/projeto-integrador-et.com/et_pontocom/public/ProdutosMP.json")
                         </div>
                         <button class="verMais">Ver Mais</button>
                     </div>
-                    <div class="popupMPcaminho>+</div>
                </div>
            </div>
        `;
        container.appendChild(card);
+
+       ///////////Abrir pop-up ao clicar em "Ver Mais"
+       const verMaisBtn = card.querySelector(".verMais");
+
+       verMaisBtn.addEventListener("click", () => {
+        const produtosMesmoDia = data.produtos.filter(p => p.dataCompra === produto.dataCompra);
+        
+        
+
+        const popupProdutos = document.getElementById("popupMP-Produtos");
+        popupProdutos.innerHTML = ""     ////limpando produtos anteriores
+
+        let totalCompra = 0;
+
+        produtosMesmoDia.forEach(p => {
+            const miniCard = document.createElement("div");
+            miniCard.classList.add("cardMini");
+
+            miniCard.innerHTML = `
+                <img src="${p.imagem}" height="110px">
+                <div class="cardMini-infos">
+                    <span class="cardMini-Titulo">${p.nome} ${p.marca}</span>
+                    <span class="cardMini-Preco">R$${parseFloat(p.preco).toFixed(2)}</span>
+                </div>
+            `;
+
+            totalCompra += parseFloat(p.preco);
+            popupProdutos.appendChild(miniCard);
+        });
+
+        document.getElementById("popupMP-DataCompra").innerText = "Data da compra: " + produto.dataCompra;
+        document.getElementById("popupMP-Total").innerText = "Total da compara: R$" +  totalCompra;
+
+        //mostrar popup
+        document.getElementById("popupMP").showModal();
+       });
+   });
+
+   //Fechar popup
+   document.querySelector('.icone-fechar').addEventListener('click', () => {
+    document.getElementById('popupMP').closest();
    });
  })
 .catch(error => {
