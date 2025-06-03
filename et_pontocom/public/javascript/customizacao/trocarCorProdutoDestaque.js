57e0e5092306dd61f1c871d67a8803cd73f4abaf
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-06-03 16:07:30 -0400

    Update trocarCorProdutoDestaque.js

--- et_pontocom/public/javascript/customizacao/trocarCorProdutoDestaque.js
+++ et_pontocom/public/javascript/customizacao/trocarCorProdutoDestaque.js
@@ -1,1 +1,3 @@
-let cor1
+let cor1 = document.getElementById('produtoLancamentoEditCor1');
+let cor2 = document.getElementById('produtoLancamentoEditCor2');
+let corSombra = document.getElementById('produtoLancamentoEditCorSombra');
