7bdd78cc261cb300e5e3abaf89b25e65a7f66806
Nícolas Marcelo Lima Eloy <134300551+nicolasmleloy@users.noreply.github.com>
2025-06-04 17:02:30 -0400

    Fix/Ajustes nas tabelas de produtos ADM e Associados

--- et_pontocom/public/componentes/tabelasAssociado_ADM/ProdutoADM/produto.php
+++ et_pontocom/public/componentes/tabelasAssociado_ADM/ProdutoADM/produto.php
@@ -1,7 +1,8 @@
 <?php
     require_once __DIR__."/../../popup/popUp.php";
     require_once __DIR__."/../../botao/botao.php";
-    
+    require_once __DIR__."/../../popup/popUp.php";
+
     function tabelaProdutoAdm($produtos){
         ?>
         <div id="lista">
@@ -14,7 +15,7 @@
                     </tr>
                 </thead>
             </table>
-    
+
             <div class="tabela-body">
                 <table id="tabelaVendas">
                     <tbody>
@@ -38,6 +39,7 @@
     }
     
     function tabelaProduto($produtos) {
+        echo PopUpComImagemETitulo("popUpSalvar","popUp_Botoes/img-confirmar.png","120px","Edição salva com sucesso!");
         ?>
         <div id="lista">
             <table id="tabelaVendas">
@@ -170,7 +172,7 @@
                     </div>
                 </div>
                 <div class="div-btn">
-                    <button class="btn-concluir-edicao">Concluír edição</button>
+                    <button onclick="abrirPopUp('popUpSalvar')" class="btn-concluir-edicao">Concluír edição</button>
                 </div>
             </dialog>
 
@@ -206,139 +208,5 @@
         </div>
         <?php
     }
-
-    // function tabelaProduto(){
-    //     return "<div id='lista'>
-    //     <table id='tabelaVendas'>
-    //         <thead id='barraCima'>
-    //             <tr>
-    //                 <th id='bordaEsquerda' scope='col'>ID</th>
-    //                 <th id='th2' scope='col'>Produto</th>
-    //                 <th id='th3' scope='col'>Associado</th>
-    //                 <th id='th4' scope='col'>SKU</th>
-    //             </tr>
-    //         </thead>
-    //     </table>
-    
-    //     <div class='tabela-body'>   
-    //         <table id='tabelaVendas'>
-    //             <tbody>
-    //                 <tr>
-    //                     <td>1</td>
-    //                     <td>Hidratante</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>S5D56GE</td>
-    //                     <!-- provavelmente vai ter que ter um botão aqui -->
-    //                 </tr>
-    //                 <tr>
-    //                     <td>2</td>
-    //                     <td>Base Liquída</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>FA9DSF56</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>3</td>
-    //                     <td>Body Splash</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>UJ47R8S</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>4</td>
-    //                     <td>Colônia Coffe Man</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>FDAS94A</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>5</td>
-    //                     <td>Skincare</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>9WE8FWS</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>6</td>
-    //                     <td>Césio Luquído</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>F99W2C9</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>7</td>
-    //                     <td>Americio de Limpeza</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>98DF5AFE8</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>8</td>
-    //                     <td>Gel de Limpeza Facial</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>V3D9S5FW8</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>9</td>
-    //                     <td>Kit Essenciais</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>GER9S8DF9</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>10</td>
-    //                     <td>Hidratante</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>S5D56GE</td>
-    //                     <!-- provavelmente vai ter que ter um botão aqui -->
-    //                 </tr>
-    //                 <tr>
-    //                     <td>11</td>
-    //                     <td>Base Liquída</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>FA9DSF56</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>12</td>
-    //                     <td>Body Splash</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>UJ47R8S</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>13</td>
-    //                     <td>Colônia Coffe Man</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>FDAS94A</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>14</td>
-    //                     <td>Skincare</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>9WE8FWS</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>15</td>
-    //                     <td>Césio Luquído</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>F99W2C9</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>16</td>
-    //                     <td>Americio de Limpeza</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>98DF5AFE8</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>17</td>
-    //                     <td>Gel de Limpeza Facial</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>V3D9S5FW8</td>
-    //                 </tr>
-    //                 <tr>
-    //                     <td>18</td>
-    //                     <td>Kit Essenciais</td>
-    //                     <td>Listar Associado</td>
-    //                     <td>GER9S8DF9</td>
-    //                 </tr>
-    //             </tbody>
-    //         </table>
-    //     </div>
-    // </div>
-    // ";
-
-    // }
 
 ?>
