4eba0fc89ddaa2e5903a2418b0a4033f1ea8d3ef
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-06-30 16:18:58 -0400

    tentando fazer merge manualmente do nicolas

---
+++ et_pontocom/public/componentes/FiltrosADMeAssociados/filtros.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+function filtro($opcoesSelect){
+    $html = '
+        <div id="divPesquisarEFiltro">
+            <div id="pesquisar">
+                <form action="">
+                    <input id="inputPesquisar" type="text" placeholder="Pesquisar Produto...">
+                </form>
+            </div>
+            <div id="filtro">
+                <select id="botaoOrdenar">
+                <option value="" selected disabled hidden>Filtro</option>';
+                
+    foreach ($opcoesSelect as $opcao) {
+        $html .= '<option value="' . htmlspecialchars($opcao) . '">' . htmlspecialchars($opcao) . '</option>';
+    }
+
+    $html .= '
+                </select>
+            </div>
+        </div>';
+
+    return $html;
+}
+?>
