686478b7a2a0e96e9a79f451830faac344e95fa1
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-07-07 16:33:33 -0400

    arrumando responsividade dos relatorios a adicionando JS

---
+++ et_pontocom/public/javascript/Associados.js
@@ -0,0 +1,33 @@
+﻿let botaoClicado = null;
+
+function preencheValidar() {
+    if (!botaoClicado) return;  
+
+    const parent = botaoClicado.closest('.verticalizacao');
+    const validarButton = parent.querySelector('.validarButton');
+    const cancelarButton = parent.querySelector('.cancelarButton');
+
+    validarButton.classList.add("marcarValido");
+    cancelarButton.classList.remove("marcarCancelar");
+
+    botaoClicado = null;
+
+    const popUp = document.getElementsByClassName("popUpConfirmar")[0];
+    popUp.close();
+}
+
+function preencheCancelar() {
+    if (!botaoClicado) return;
+
+    const parent = botaoClicado.closest('.verticalizacao');
+    const validarButton = parent.querySelector('.validarButton');
+    const cancelarButton = parent.querySelector('.cancelarButton');
+
+    cancelarButton.classList.add("marcarCancelar");
+    validarButton.classList.remove("marcarValido");
+
+    botaoClicado = null;
+
+    const popUp = document.getElementsByClassName("popUpCancelar")[0];
+    popUp.close();
+}
