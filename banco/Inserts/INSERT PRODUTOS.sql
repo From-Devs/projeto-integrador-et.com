1d0260f4f63353cb551aefeeebdf4282945ea2ca
Nícolas Marcelo Lima Eloy <134300551+nicolasmleloy@users.noreply.github.com>
2025-08-13 15:15:41 -0400

    fix/Ajuste de arquivos do banco

--- banco/Inserts/INSERT PRODUTOS.sql
+++
@@ -1,8 +0,0 @@
--- Hidratante Corporal Milk
-
-INSERT INTO `produto`(`nome`, `marca`, `descricaoBreve`, `descricaoTotal`, `preco`, `precoPromo`, `imagem`, `id_subCategoria`, `id_cores`, `id_associado`)
-VALUES
-('Hidratante Corporal Milk','Nivea','Descrição breve','Descrição total',29.90,19.90,'milk.png',10,1,2),
-('Body Splash Biscoito ou Bolacha','O Boticário','Descrição breve','Descrição total',29.90,19.90,'biscoito.png',22,2,2),
-('Base Líquida Efeito Matte','Vult','Descrição breve','Descrição total',29.90,19.90,'vult.png',1,3,2),
-('Colonia Coffe Man','O Boticário','Descrição breve','Descrição total',29.90,19.90,'coffe.png',6,4,2);
--- banco/Inserts/inserts.sql
+++ banco/Inserts/inserts.sql
@@ -63,3 +63,19 @@ VALUES
 ('Óleos', 6),
 ('Cremes', 6),
 ('Protetores', 6);
+
+INSERT INTO `cores`(`corPrincipal`, `hexDegrade1`, `hexDegrade2`)
+VALUES
+('#3E7FD9','#133285','#3F7FD9'),
+('#31BADA','#00728C','#31BADA'),
+('#DBA980','#72543A','#E4B186'),
+('#D2936A','#6C4A34','#D29065')
+
+-- Hidratante Corporal Milk
+
+INSERT INTO `produto`(`nome`, `marca`, `descricaoBreve`, `descricaoTotal`, `preco`, `precoPromo`, `imagem`, `id_subCategoria`, `id_cores`, `id_associado`)
+VALUES
+('Hidratante Corporal Milk','Nivea','Descrição breve','Descrição total',29.90,19.90,'milk.png',10,1,2),
+('Body Splash Biscoito ou Bolacha','O Boticário','Descrição breve','Descrição total',29.90,19.90,'biscoito.png',22,2,2),
+('Base Líquida Efeito Matte','Vult','Descrição breve','Descrição total',29.90,19.90,'vult.png',1,3,2),
+('Colonia Coffe Man','O Boticário','Descrição breve','Descrição total',29.90,19.90,'coffe.png',6,4,2);
