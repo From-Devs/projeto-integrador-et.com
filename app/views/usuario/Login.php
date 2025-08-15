8e7fefe18ccbe71f4efb9d64425e9678345329aa
DanielPydev <danielprosperodefigueiredo@gmail.com>
2025-08-15 16:59:54 -0400

    Respo. 02

--- app/views/usuario/Login.php
+++ app/views/usuario/Login.php
@@ -38,7 +38,7 @@
         <section>
             <div class='wave solida'></div>
             <picture>
-                <source media="(max-width: 1240px)" srcset="/projeto-integrador-et.com/public/imagens/ET/LogoPreta1.png">
+                <source media="(max-width: 870px)" srcset="/projeto-integrador-et.com/public/imagens/ET/LogoPreta1.png">
                 <img id="fotoET" src="/projeto-integrador-et.com/public/imagens/ET/LogoBranca1.png" alt="">
             </picture>
         </section>
