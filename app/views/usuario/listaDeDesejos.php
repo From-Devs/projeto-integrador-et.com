f1293db5f7cb394c7dc4ae149670d6cdcd4ba8b4
Nicolle Zaleski <nicolle.m.zaleski@gmail.com>
2025-08-21 15:31:32 -0400

    cardListadeDestaques
    css feito, falta configurar a parte de selecionar todas as checkboxes

--- app/views/usuario/listaDeDesejos.php
+++ app/views/usuario/listaDeDesejos.php
@@ -63,7 +63,7 @@
         </div>
     </div> -->
    
-    <div id="acoesSelecionados" style="display:none;">
+    <div class="acoesSelecionados" id="acoesSelecionados" style="display:none;">
         <input type="checkbox" id="selecionarTodos"> Selecionar todos
         <button id="adicionarCarrinho">Adicionar no carrinho</button>
         <button id="excluirSelecionados">Excluir</button>
@@ -74,29 +74,43 @@
         <div class="degradeBaixo"></div>
         <div class="card-container">
             <?php 
+                echo createCardListaDeDesejos(1,"bt-ovni.png",48.68,"Bruna Tavares", "BT Ovni Galaxy","08/07/2025", "rgba(28, 30, 37, 0.712)","rgb(217,234,37)", "rgb(221, 235, 67)");
+                echo createCardListaDeDesejos(2,"superstay-ink-vinyl.png",99.51,"Maybelline", "Superstay Vinyl Ink Liquid Lipstick","08/07/2025", "rgb(160, 1, 27)","rgb(199, 43, 69)", "rgb(211, 112, 128)");
+                echo createCardListaDeDesejos(3,"base-liquida-matte-vult.png",23.87,"Vult", "Base Líquida Efeito Matte","08/07/2025", "rgb(197, 153, 114)","rgb(231,187,148)", "rgb(241, 204, 171)");
+                echo createCardListaDeDesejos(4,"bt-velvet-blackberry.png",35.89,"Bruna Tavares", "BT Velvet Blackberry","08/07/2025", "rgb(58, 9, 13)","rgb(112, 37, 42)", "rgb(179, 110, 116)");
+                echo createCardListaDeDesejos(5,"renew-avon.png",75.90,"Avon", "Creme Renew Reversalist Dia Vitalidade 30+","08/07/2025", "rgb(143, 43, 41)","rgb(182, 78, 76)", "rgb(196, 117, 116)");
+                echo createCardListaDeDesejos(6,"bodysplash-cuide-se-bem.png",84.90,"O Boticário", "Body Splash Cuide-se Bem Nuvem","08/07/2025", "rgb(139, 198, 206)","rgb(176, 237, 247)", "rgb(205, 245, 250)");
+                echo createCardListaDeDesejos(7,"epidrat-mantecorp-facial.png",66.60,"Mantecorp", "Epidrat Calm Hidratante","08/07/2025", "rgb(81,74,108)","rgb(149, 140, 185)", "rgb(163, 156, 189)");
+                echo createCardListaDeDesejos(8,"hidratante-nivea.png",20.99,"Nivea", "Creme Hidratante Milk","08/07/2025", "rgb(15, 44, 122)","rgb(70, 100, 182)", "rgb(117, 138, 194)");
+                echo createCardListaDeDesejos(9,"Malbec_Colonia.png",169.00,"O Boticário", "Malbec Tradicional","08/07/2025", "rgb(65, 16, 16)","rgb(102, 56, 48)", "rgb(122, 92, 85)");
+                echo createCardListaDeDesejos(10,"pincel-marimaria.png",95.90,"Mari Maria Makeup", "Pincel Angular Para Base","08/07/2025", "rgb(187, 49, 1)","rgb(232, 104, 63)", "rgb(235, 149, 120)");
+                echo createCardListaDeDesejos(11,"esponja-mari-maria.png",35.90,"Mari Maria Makeup", "Esponja Flat Blende","08/07/2025", "rgb(241, 93, 10)","rgb(243, 130, 64)", "rgb(248, 180, 140)");
+                echo createCardListaDeDesejos(12,"truss_net_masc.png",269.99,"Truss", "Net Mask Máscara Capilar","08/07/2025", "rgb(0, 150, 177)","rgb(66, 203, 228)", "rgb(141, 221, 235)");
+                echo createCardListaDeDesejos(13,"amor-amor-perfume-feminino.png",405.30,"Cacharel", "AMOR AMOR","08/07/2025", "rgb(206, 21, 21)","rgb(247, 53, 53)", "rgb(255, 255, 255)");
+                
                 // if ($id_usuario) {
-                    $sql = "SELEC p.id_produto, p.nome, p.marca, p.preco, p.precoPromo, p.imagem, ld.dataAdd
-                            FROM ListaDesejos ld
-                            JOIN Produto p ON ld.id_produto = p.id_produto
-                            WHERE ld.id_usuario = ?";
-                    $stmt = $conn->prepare($sql);
-                    $stmt->execute([$id_usuario]);
-                    $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    // $sql = "SELEC p.id_produto, p.nome, p.marca, p.preco, p.precoPromo, p.imagem, ld.dataAdd
+                    //         FROM ListaDesejos ld
+                    //         JOIN Produto p ON ld.id_produto = p.id_produto
+                    //         WHERE ld.id_usuario = ?";
+                    // $stmt = $conn->prepare($sql);
+                    // $stmt->execute([$id_usuario]);
+                    // $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                    if ($produtos){
-                        foreach ($produtos as $produto){
-                            echo createCardListaDeDesejos(
-                                $produto['id_produto'],
-                                $produto['imagem'],
-                                $produto['precoPromo'] ?? $produto['preco'],
-                                $produto['marca'],
-                                $produto['nome'],
-                                date("d/m/Y", strtotime($produto['dataAdd']))
-                            );
-                        }
-                    } else{
-                        echo "<span>Sua lista de desejos está vazia.</span>";
-                    }
+                    // if ($produtos){
+                    //     foreach ($produtos as $produto){
+                    //         echo createCardListaDeDesejos(
+                    //             $produto['id_produto'],
+                    //             $produto['imagem'],
+                    //             $produto['precoPromo'] ?? $produto['preco'],
+                    //             $produto['marca'],
+                    //             $produto['nome'],
+                    //             date("d/m/Y", strtotime($produto['dataAdd']))
+                    //         );
+                    //     }
+                    // } else{
+                    //     echo "<span>Sua lista de desejos está vazia.</span>";
+                    // }
                 // } else{
                 //     echo "<span>Você precisa estar logado para adicionar e ver sua lista de desejo.</span>";
                 // }
--- public/componentes/cardListaDeDesejos/cardListaDeDesejos.php
+++ public/componentes/cardListaDeDesejos/cardListaDeDesejos.php
@@ -3,7 +3,7 @@
     function createCardListaDeDesejos( $id_produto,$imagemProd = "", $preco = 00.00, $marca = "", $nome = "", $dataAdicionado = "", $cor1 = "#919191", $cor2 = "#919191", $cor3 = "#919191"){
         return "
             <div class='cardDesejos'>
-                <div>
+                <div class='checkbox'>
                     <input type='checkbox' class='cardCheckbox' data-id='$id_produto'>
                 </div>
                 <div class='cardColorido'  style='background: linear-gradient(to right, $cor1, $cor2, $cor3, #FFFFFF);'>
@@ -11,7 +11,7 @@
                         <img src='/projeto-integrador-et.com/public/imagens/ProdutosMP/$imagemProd' alt='$nome'>
                     </div>
                     <span class='cardPreco'>R$ ".number_format($preco, 2, ',', '.')."</span>
-                    <span class='cardInfo'>$marca $nome</span>
+                    <span class='cardInfo'>$nome $marca</span>
                     <span class='cardDate'>Adicionado $dataAdicionado</span>
                     <div class='buttonsListaDesejos'>
                         <button class='buttonCarrinho' id='buttonCarrinho' onclick='redirecionarCarrinho()'>
