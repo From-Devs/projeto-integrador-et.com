e327b07cb16786e31e704350a59cc0ee4c22a8ae
Nicolle Zaleski <nicolle.m.zaleski@gmail.com>
2025-09-01 15:03:49 -0400

    Alterei o componente
    Adicionei a opção de colocar produto com preço de promoção

--- public/componentes/cardListaDeDesejos/cardListaDeDesejos.php
+++ public/componentes/cardListaDeDesejos/cardListaDeDesejos.php
@@ -1,5 +1,16 @@
 <?php  
-    function createCardListaDeDesejos( $id_produto,$imagemProd = "", $preco = 00.00, $marca = "", $nome = "", $dataAdicionado = "", $corprincipal = "#919191", $corhexdegrade1 = "#919191", $corhexdegrade2 = "#919191", $corhexdegrade3 = "#919191"){
+    function createCardListaDeDesejos( $id_produto,$imagemProd = "", $preco = 00.00, $marca = "", $nome = "", $dataAdicionado = "", $corprincipal = "#919191", $corhexdegrade1 = "#919191", $corhexdegrade2 = "#919191", $corhexdegrade3 = "#919191", $precoPromo=null){
+
+        if (!empty($precoPromo) && $precoPromo < $preco){
+            $precoCarrinho = "
+            <span class='precoAntigo'>R$ ".number_format($preco, 2, ',', '.')."</span>
+            <span class='precoNovo'>R$ ".number_format($precoPromo, 2, ',', '.')."</span>
+            ";
+        } else{
+            $precoCarrinho = "
+                <span class='cardPreco'>R$ ".number_format($preco, 2, ',', '.')."</span>
+            ";
+        }
         return "
             <div class='cardDesejos' data-id='$id_produto'>
                 <div class='checkboxes'>
@@ -9,7 +20,9 @@
                     <div class='cardImg'>
                         <img src='/projeto-integrador-et.com/public/imagens/produtos/$imagemProd/fotoSemFundo.png' alt='$nome'>
                     </div>
-                    <span class='cardPreco'>R$ ".number_format($preco, 2, ',', '.')."</span>
+                    <span class='cardPrecoArea'>
+                        {$precoCarrinho}
+                    </span>
                     <span class='cardInfo'>$nome $marca</span>
                     <span class='cardDate'>Adicionado $dataAdicionado</span>
                     <div class='buttonsListaDesejos'>
