b86de7730a30149b8b0a43aa029da3ecc7cda4b0
GuilhermeNantes <guinantes27@gmail.com>
2025-09-02 17:09:09 -0400

    volter que corrigir

--- app/Controllers/ProdutoController.php
+++ app/Controllers/ProdutoController.php
@@ -1,53 +1,19 @@
 <?php
-require_once __DIR__ . "/../../config/database.php";
+require_once __DIR__ . '/../Models/products.php';
 
-class ProdutoController{
-
-    private $conn;
-
-    public function __construct(){
-        $banco = new Database();
-
-        $this->conn = $banco->Connect();
+class UserController {
+    private $produtomodel;
+    
+    public function __construct() {
+        $this->produtomodel = new Products();
     }
+
     public function buscarTodosProdutos(){
         return $this->produtoModel->buscarTodosProdutos();
     }
     
     public function buscarPorId($id) {
         return $this->produtoModel->buscarProdutoPorId($id);
-    }
-   
-    // public function EditarProduto(){
-        
-    // }
-
-    // public function CadastrarProduto($nome, $marca, $breveDescricao, $preco, $precoPromocional, $caracteristicasCompleta, $qtdEstoque, $img1, $img2, $img3) Com imagem cadastrando (Blob) - Verificar qual tipo salvar no banco
-    public function CadastrarProduto($nome, $marca, $breveDescricao, $preco, $precoPromocional, $caracteristicasCompleta, $qtdEstoque){
-        try {
-            // $sql = "INSERT INTO PRODUTO(nome, marca, descricaoBreve, descricaoTotal, preco, precoPromo, qtdEstoque, img1, img2, img3)
-            //(:nome, :marca, :descricaoBreve, :descricaoTotal, :preco, :precoPromo, :qtdEstoque, :img1, :img2, :img3)";
-            $sql = "INSERT INTO PRODUTO(nome, marca, descricaoBreve, descricaoTotal, preco, precoPromo, qtdEstoque)
-            VALUES
-            (:nome, :marca, :descricaoBreve, :descricaoTotal, :preco, :precoPromo, :qtdEstoque)";   
-            $db = $this->conn->prepare($sql);
-            $db->bindParam(":nome",$nome);
-            $db->bindParam(":marca",$marca);
-            $db->bindParam(":descricaoBreve",$breveDescricao);
-            $db->bindParam(":preco",$preco);
-            $db->bindParam(":precoPromo",$precoPromocional);
-            $db->bindParam(":qtdEstoque",$qtdEstoque);
-            // $db->bindParam(":img1",$img1);
-            // $db->bindParam(":img2",$img2);
-            // $db->bindParam(":img3",$img3);
-            $db->bindParam(":descricaoTotal",$caracteristicasCompleta);
-            $resposta = $db->execute();
-
-            return $resposta;
-        } catch (\Throwable $th) {
-            //throw $th;
-            echo $th->getMessage();
-        }
     }
     
     public function removerProduto($id_delete){
