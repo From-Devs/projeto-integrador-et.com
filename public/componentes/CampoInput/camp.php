744b03feff6948341e160ccb0e8904926fe6ddbe
Robert Fernandes <robertbetinho.filho@gmail.com>
2025-09-15 17:14:54 -0400

    coisando as coisa
    fazendo a parte de solicitação de associado

--- public/componentes/CampoInput/camp.php
+++ public/componentes/CampoInput/camp.php
@@ -1,11 +1,11 @@
 <?php
-function Camp($label, $tipo="text", $name=null, $class="campo") {
+function Camp($label, $tipo="text", $name=null, $class="campo", $value = "") {
     $id = $name ?? strtolower(str_replace(' ', '_', $label));
     $asterisco = "<b id='red'>*</b>";
 
     return "
     <div class='{$class}-campo-container'>
-        <input type='{$tipo}' name='{$id}' id='{$id}' class='input' required placeholder=' '>
+        <input type='{$tipo}' name='{$id}' id='{$id}' class='input' required placeholder=' ' value='" . htmlspecialchars($value, ENT_QUOTES) . "'>
         <label for='{$id}'>{$label} {$asterisco}</label>
     </div>";
 }
