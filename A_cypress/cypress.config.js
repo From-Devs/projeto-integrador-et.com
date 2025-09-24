18045801407b533c01805a863f0e6649bacc37de
ElielZinKwaiProGamer123 <elielmurbach01@gmail.com>
2025-09-24 15:30:11 -0400

    Cypress da tela de Cadastro Associado Feito🔥🔥🔥

--- A_cypress/cypress.config.js
+++ A_cypress/cypress.config.js
@@ -5,8 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:"http://localhost/projeto-integrador-et.com/app/views/usuario/"
-    // baseUrl:"http://localhost/projeto-integrador-et.com/app/views/associado/"
-    // baseUrl:"http://localhost/projeto-integrador-et.com/app/views/adm/"
-  },
+    baseUrl:"http://localhost/projeto-integrador-et.com/app/views/usuario/",
+    },
 });
