533417d4ad7a71c5452345da07363011a06c4034
GuilhermeNantes <guinantes27@gmail.com>
2025-10-13 17:16:58 -0400

    feat: adicaçao create carousel

--- app/Controllers/CarouselController.php
+++ app/Controllers/CarouselController.php
@@ -27,22 +27,23 @@ class CarouselController {
     public function update(int $id, int $date): array{
         if (!$id || empty($data)) {
             return false;
-        }
+        };
         return $this->carouselModel->updateById($id, $data);
     }
     public function getId(int $id): array{
         return $this->carouselModel->getById($id);
     }
-}
+};
 
 // 🔹 Teste rápido  
 $controller = new CarouselController();
 $all = $controller->getAll();
 print_r($all);
+echo $all
 $new = $controller->create([
     "id_carousel" => 1,
-    "id_produto" => 1,
+    "id_produto" => 7,
     "id_coresSubs" => 1
 ]);
-print_r($new) // true ou false
+print_r($new); // true ou false
 ?>
