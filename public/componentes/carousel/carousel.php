57bc6d605b4de422cea961f95555e3a589bcf244
Guilherme Nantes <guinantes27@gmail.com>
2025-10-23 17:09:42 -0400

    carrosei

--- public/componentes/carousel/carousel.php
+++ public/componentes/carousel/carousel.php
@@ -1,31 +1,36 @@
 <?php
 session_start();
 $dados = $_SESSION['dados_carrossel'] ?? [];
-extract($dados , EXTR_SKIP);
+$carousels = $dados['carousels'] ?? [];
 
+// echo '<img src="../../../' . htmlspecialchars($carousels[0]['img1']) . '" />';
 require __DIR__ . "/../../../public/componentes/carouselPopUp/carouselPopUp.php";
-function createCarousel($carrosel){
-    // echo '<img src="../../../' . $itens['img1'] . '" />';
-    return '<div class="carousel">
+function createCarousel($carousels){
+ $html = '<div class="carousel">
+    <div class="carousel-track" id="MoverCarrousel">'
+    . createCarouselPopUp();
+foreach ($carousels as $cs) {
+    $img = $cs['img1'] ?? 'public/uploads/padrao.jpg';
+    $html .= '
+        <div class="carousel-item">
+            <img src="/' . htmlspecialchars($img) . '" alt="">
+        </div>';
+}
 
-        <div class="carousel-track" id="MoverCarrousel">
-            '. createCarouselPopUp();
-            foreach ($carrosel as $cs) {
-                '<div class="carousel-item">
-                    <img src="../../../' . $cs['img1'] . '" />
-                </div>'
-            }; . '"
+$html .= '
+    </div>
+    <div class="bottom-controls">
+        <button class="Seta-btn" id="prev"><i class="fas fa-chevron-left"></i></button>
+        <div class="Bolas" id="BolasContainer">
+            <div class="Bola"></div>
+            <div class="Bola"></div>
+            <div class="Bola"></div>
         </div>
-        <div class="bottom-controls">
-            <button class="Seta-btn" id="prev"><i class="fas fa-chevron-left"></i></button>
-            <div class="Bolas" id="BolasContainer">
-                <div class="Bola"></div>
-                <div class="Bola"></div>
-                <div class="Bola"></div>
-            </div>
-            <button class="Seta-btn" id="next"><i class="fas fa-chevron-right"></i></button>
-        </div>
-    </div>';
+        <button class="Seta-btn" id="next"><i class="fas fa-chevron-right"></i></button>
+    </div>
+</div>';
+
+return $html;
     
 }
-echo createCarousel($carousels);
+
