f55f974908bb6be255393c5e67b8fda35969aa62
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-10-27 17:15:26 -0400

    contiuando previsu

--- public/javascript/previsualizacaoProdutoCadastroEditar.js
+++ public/javascript/previsualizacaoProdutoCadastroEditar.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const botaoCadastroPopUp = document.getElementById('botaoAssociados');
     const acoesTabela = document.querySelectorAll(".acoes-tabela");
+
+    function gerarPorcentagemDesconto(precoDoProdutoOriginal, precoDoProdutoDesconto){
+        let porcentagemDesconto = (((precoDoProdutoOriginal - precoDoProdutoDesconto)/precoDoProdutoOriginal)*100);
+        porcentagemDesconto = parseInt(porcentagemDesconto);
+        porcentagemCardProduto.innerHTML = porcentagemDesconto + "%";
+    }
     
     function atualizarDadosProdutosPopUp(tipoPopUp = "cadastro"){
-        observer.disconnect()
+        // observer.disconnect()
         if (tipoPopUp == "editar") {
             popUp = document.querySelector(".dialog-editar");
         }else{
@@ -22,46 +28,48 @@ document.addEventListener("DOMContentLoaded", () => {
         const corPrincipal = formProduto.querySelector('input[name="corPrincipal"]');
         const degrade1 = formProduto.querySelector('input[name="deg1"]');
         const degrade2 = formProduto.querySelector('input[name="deg2"]');
+        const imagemProduto = formProduto.querySelectorAll('.imagem-produto')[0];
         
         // Campos do Card de produto
         const cardProduto = formProduto.querySelector('.cardProduto');
         const nomeCardProduto = cardProduto.querySelector('.nomeProduto');
         const marcaCardProduto = cardProduto.querySelector('.marca');
-        const precoCardProduto = cardProduto.querySelector('.preco');
-        const precoPromoCardProduto = cardProduto.querySelector('.precoOriginal');
+        const precoAtualCardProduto = cardProduto.querySelector('.preco');
+        const precoOriginalCardProduto = cardProduto.querySelector('.precoOriginal');
+        const porcentagemCardProduto = cardProduto.querySelector('.descontoPorcento');
         const corPrincipalCardProduto = cardProduto.querySelector('.corDestaque');
         const degrade1CardProduto = cardProduto.querySelector('.corDegrade1');
         const degrade2CardProduto = cardProduto.querySelector('.corDegrade2');
+        const imagemCardProduto = cardProduto.querySelector('.imagemCardProdutoComum');
         const botaoComprar = cardProduto.querySelector(".botaoComprarCardProduto");
         const botaoAnimacao = cardProduto.querySelector(".botaoEspectro");
-        // console.log(cardProduto);
+        // console.log(imagemProduto);
         // console.log(nomeCardProduto);
         // console.log(marcaCardProduto);
-        // console.log(precoCardProduto);
-        // console.log(precoPromoCardProduto);
-    
+        // console.log(precoAtualCardProduto);
+        // console.log(precoOriginalCardProduto);
+
+        
         if (promoCheckboxProduto.checked) {
             if (cardProduto.className == "cardProduto") {
                 cardProduto.classList.add("desconto");
             }
+            precoAtualCardProduto.innerHTML = "R$" + precoPromoProduto.value;
+            precoOriginalCardProduto.innerHTML = "R$" + precoProduto.value;
+
+            let porcentagemDesconto = (((precoProduto.value - precoPromoProduto.value)/precoProduto.value)*100);
+            porcentagemDesconto = parseInt(porcentagemDesconto);
+            porcentagemCardProduto.innerHTML = porcentagemDesconto + "%";
         }else{
             if (cardProduto.className == "cardProduto desconto") {
                 cardProduto.classList.remove("desconto");
             }
+            precoAtualCardProduto.innerHTML = "R$" + precoProduto.value;
         }
-    
-        promoCheckboxProduto.addEventListener("input", function(){
-            if (promoCheckboxProduto.checked) {
-                cardProduto.classList.add("desconto");
-            }else{
-                cardProduto.classList.remove("desconto");
-            }
-        })
 
         nomeCardProduto.innerHTML = nomeProduto.value;
         marcaCardProduto.innerHTML = marcaProduto.value;
-        precoCardProduto.innerHTML = "R$" + precoProduto.value;
-        precoPromoCardProduto.innerHTML = "R$" + precoPromoProduto.value;
+        imagemCardProduto.src = imagemProduto.src;
         corPrincipalCardProduto.style.color = corPrincipal.value;
         degrade1CardProduto.style.color = degrade1.value;
         degrade2CardProduto.style.color = degrade2.value;
@@ -79,33 +87,68 @@ document.addEventListener("DOMContentLoaded", () => {
         botaoComprar.style.backgroundImage = "linear-gradient(to top, "+ cores[1] +" 0%, "+ cores[2] +" 75%)";
         botaoAnimacao.style.backgroundImage = "linear-gradient(to top, "+ cores[1] +" 0%, "+ cores[2] +" 75%)";
 
+        nomeProduto.addEventListener("input", function(){
+            nomeCardProduto.innerHTML = nomeProduto.value;
+        })
+
+        marcaProduto.addEventListener("input", function(){
+            marcaCardProduto.innerHTML = marcaProduto.value;
+        })
+
+        precoProduto.addEventListener("input", function(){
+            if (promoCheckboxProduto.checked) {
+                precoOriginalCardProduto.innerHTML = "R$" + precoProduto.value
+            }else{
+                precoAtualCardProduto.innerHTML = "R$" + precoProduto.value;
+            }
+        })
+
+        precoPromoProduto.addEventListener("input", function(){
+            if (promoCheckboxProduto.checked) {
+                precoAtualCardProduto.innerHTML = "R$" + precoPromoProduto.value
+            }
+        })
+    
+        promoCheckboxProduto.addEventListener("input", function(){
+            if (promoCheckboxProduto.checked) {
+                cardProduto.classList.add("desconto");
+                precoAtualCardProduto.innerHTML = "R$" + precoPromoProduto.value;
+                precoOriginalCardProduto.innerHTML = "R$" + precoProduto.value;
+            }else{
+                cardProduto.classList.remove("desconto");
+                precoAtualCardProduto.innerHTML = "R$" + precoProduto.value;
+            }
+        })
+
+
+
         
-        observer.observe(document.body, { childList: true, subtree: true});
+        // observer.observe(document.body, { childList: true, subtree: true});
     }
 
-    const observer = new MutationObserver(() => {
-        const popUpEditar = document.querySelector(".dialog-editar[open]");
-        const popUpCadastrar = document.querySelector(".dialog-cadastrar[open]");
+    // const observer = new MutationObserver(() => {
+    //     const popUpEditar = document.querySelector(".dialog-editar[open]");
+    //     const popUpCadastrar = document.querySelector(".dialog-cadastrar[open]");
 
-        if (popUpEditar){
-            atualizarDadosProdutosPopUp("editar");
-        } 
-        if(popUpCadastrar){
-            atualizarDadosProdutosPopUp();
-        }
-    })
-
-    observer.observe(document.body, { childList: true, subtree: true});
-
-
-    // acoesTabela.forEach((botoes) => {
-    //     botoes.querySelector(".editar").addEventListener("click", () => {
-    //         setTimeout(() => {atualizarDadosProdutosPopUp("editar")}, 50)
-            
-    //     });
+    //     if (popUpEditar){
+    //         atualizarDadosProdutosPopUp("editar");
+    //     } 
+    //     if(popUpCadastrar){
+    //         atualizarDadosProdutosPopUp();
+    //     }
     // })
 
-    // botaoCadastroPopUp.addEventListener("click", () => {
-    //     atualizarDadosProdutosPopUp();
-    // });
+    // observer.observe(document.body, { childList: true, subtree: true});
+
+
+    acoesTabela.forEach((botoes) => {
+        botoes.querySelector(".editar").addEventListener("click", () => {
+            setTimeout(() => {atualizarDadosProdutosPopUp("editar")}, 50)
+            
+        });
+    })
+
+    botaoCadastroPopUp.addEventListener("click", () => {
+        atualizarDadosProdutosPopUp();
+    });
 });
