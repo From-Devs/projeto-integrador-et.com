1a9e9a2b63c4d2e036c7994aa1e53991b614f0f9
Guilherme Nantes <guinantes27@gmail.com>
2025-11-18 17:13:06 -0400

    tendo uma crise de idade
    to junando tudo no custimizaçao

--- app/Controllers/CustomizacaoController.php
+++ app/Controllers/CustomizacaoController.php
@@ -2,17 +2,37 @@
 // Controllers/CustomizacaoModel.php
 require_once __DIR__ . "/../Models/CustomizacaoModel.php";
 
-class CustomizacaoController
-{
-    public function index(){
-        $model = new CustomizacaoModel()
-        // get alguma coisa
-        $usuarios = $model->read()
+require_once __DIR__ . "/../Models/DestaqueModel.php";
+//require_once __DIR__ . "/../Models/LancamentoModel.php";
+class CustomizacaoController(){
+    // model juntando toda em unicico arquivo
+    private ProdutoDestaque $DestaqueModel
+    private CarouselModel $CarouselModel
+    // private ------------- $LansamenModel
+    private CoresSubModel $CoreSubsModel
 
-        
-        echo "<pre>";
-        print_r($usuarios);
-        echo "</pre>";
+    public function __construct() {
+        $this->DestaqueModel = new DestaqueModel();
+        $this->CarouselModel = new CoresSubModel();
+        // $this->LansamenModel = new LancamentoModel();
+        $this->CoreSubsModel = new CoresSubModel();;
     }
+
+    public function Create_Carousel(int $id,array $data):array {
+        $res =  $CarouselModel->create($id,$data)
+        return $res
+    }
+    public function Create_Destaque(int $id,array $data): array {
+        $res =  $DestaqueModel->create($id,)
+        return $res
+    }
+    // public function Create_Lancamentos() { "
+    
+    // }
+    public function Create_CoresSubs() {
+        $res =  $CoreSubsModel->create($id,)
+        return $res
+    }
+
 }
 ?>
