213b972f707644cb0ba3591d192ed53bb127d6c1
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-11-27 14:59:04 -0400

    Update DestaqueModel.php

--- app/Models/DestaqueModel.php
+++ app/Models/DestaqueModel.php
@@ -31,59 +31,6 @@ class ProdutoDestaque {
     }
 
   }  
-  public function create(int $id, array $data): bool {
-    try{
-      if(empty($data["id_produto"])||  !is_numeric($data["id_produto"])) {
-        return ['error' => "ID do produto inválido ou ausente."];
-      } 
-      $id_produto = (int) $data["id_protudo"];
-      if($this->productExists($id_produto)){
-        return ['error' => 'Produto não encontrado.'];
-      }
-      $this->conn->beginTransaction();
-      $stmt = $this->conn->query('SELECT COUNT(*) as total from proddestaque');
-      $total = (int) $stmt->fetch()['total'];
-      if($total > 1){
-        $this->conn->rollBack();
-        return ['error' => 'Limite atingido, não é possível criar mais de 1 protudo destaque.'];
-      }
-      $stmt = $this->conn->prepare("SELECT corEspecial, hexDegrade1, hexDegrade2, hexDegrade3, id_cores FROM produto WHERE id_produto = :id_produto");
-      $stmt->execute([':id_produto' => $id_produto]);
-      $produto = $stmt->fetch(PDO::FETCH_ASSOC);
-      if (!$produto) {
-        $this->conn->rollBack();
-        return ['error' => 'Produto não encontrado para copiar cores.'];
-      }
-      $cores = [
-       'corEspecial' => $produto['corEspecial'] ?? null,
-        'hexDegrade1' => $produto['hexDegrade1'] ?? null,
-        'hexDegrade2' => $produto['hexDegrade2'] ?? null,
-        'hexDegrade3' => $produto['hexDegrade3'] ?? null,
-      ];
-      if (empty($cores['corEspecial']) && !empty($produto["id_cores"])){
-        $stmt = $this->conn->prepare('SELECT corPrincipal AS corEspecial, hexDegrade1, hexDegrade2 FROM cores WHERE id_cores = :id LIMIT 1');
-      }
-
-    }catch (PDOException $e) {
-        error_log("[proddestaque] Erro ao criar: " . $e->getMessage());
-        return false;
-    }
-
-    public function getAll(): array {
-        try {
-            $sql = "
-                SELECT pd.id_proddestaque, pd.cor1, pd.cor2, p.id_produto, p.nome, p.marca, p.preco, p.precoPromo, p.img1, p.fgPromocao
-                FROM proddestaque pd
-                JOIN produto p ON p.id_produto = pd.id_produto
-            ";
-            $stmt = $this->conn->query($sql);
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            error_log("[ProdutoDestaque] Erro SQL getAll: " . $e->getMessage());
-            return [];
-        }
-    }
-
 
     public function create($id, array $data) {
         try {
